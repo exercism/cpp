7d7460b89e51b87d003de71b3c698539e36b516a
Richard <legalize@xmission.com>
2014-07-28 03:12:11 -0600

    Pass food-chain

---
+++ food-chain/example.h
@@ -0,0 +1,15 @@
+#if !defined(FOOD_CHAIN_H)
+#define FOOD_CHAIN_H
+
+#include <string>
+
+namespace food_chain
+{
+
+extern std::string verse(unsigned which);
+extern std::string verses(unsigned begin, unsigned end);
+extern std::string sing();
+
+}
+
+#endif
