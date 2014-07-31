22a47059d81d38a2bf203916d4e27d9d2e754ec1
Richard <legalize@xmission.com>
2014-07-30 22:33:37 -0600

    Pass robot-name

---
+++ robot-name/example.h
@@ -0,0 +1,24 @@
+#if !defined(ROBOT_NAME_H)
+#define ROBOT_NAME_H
+
+#include <string>
+
+namespace robot_name
+{
+
+class robot
+{
+public:
+    robot();
+
+    std::string name() const { return name_; }
+
+    void reset();
+
+private:
+    std::string name_;
+};
+
+}
+
+#endif
