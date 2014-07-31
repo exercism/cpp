db9211a888c2955a1b90e965d43427ffdd7ba8d5
Richard <legalize@xmission.com>
2014-07-30 23:28:24 -0600

    Pass etl

---
+++ etl/example.cpp
@@ -0,0 +1,21 @@
+#include "etl.h"
+
+#include <cctype>
+
+using namespace std;
+
+namespace etl
+{
+
+extern map<char, int> etl::transform(map<int, vector<char>> const& old)
+{
+    map<char, int> result;
+    for (auto const& item : old) {
+        for (char c : item.second) {
+            result[tolower(c)] = item.first;
+        }
+    }
+    return result;
+}
+
+}
