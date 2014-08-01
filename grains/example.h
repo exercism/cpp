f28795733249dbea66cf1659ae684640aeeb5541
Richard <legalize@xmission.com>
2014-07-31 19:06:00 -0600

    Pass grains

---
+++ grains/example.h
@@ -0,0 +1,17 @@
+#if !defined(GRAINS_H)
+#define GRAINS_H
+
+namespace grains
+{
+
+class grains
+{
+public:
+    unsigned long long square(int which) const;
+
+    unsigned long long total() const;
+};
+
+}
+
+#endif
