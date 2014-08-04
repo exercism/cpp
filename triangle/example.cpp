6eefb56b8e09def597b22b48b84814941a2feb96
Richard <legalize@xmission.com>
2014-08-04 03:05:34 -0600

    Switch triangle to simple function

--- triangle/example.cpp
+++ triangle/example.cpp
@@ -1,6 +1,9 @@
 #include "triangle.h"
 #include <stdexcept>
 
+namespace triangle
+{
+
 namespace
 {
 
@@ -11,34 +14,23 @@ bool triangle_equality(double a, double b, double c)
         && c < (a + b);
 }
 
-std::string determine_triangle_kind(double a, double b, double c)
+}
+
+extern flavor kind(double a, double b, double c)
 {
     if (a == 0 || b == 0 || c == 0) {
-        return "";
-    }
-    if (a < 0 || b < 0 || c < 0 || !triangle_equality(a, b, c)) {
-        return "illegal";
-    }
-    if (a == b && b == c) {
-        return "equilateral";
-    }
-    if (a == b || b == c || a == c) {
-        return "isosceles";
-    }
-    return "scalene";
-}
-
-}
-
-triangle::triangle(double a, double b, double c)
-    : kind_(determine_triangle_kind(a, b, c))
-{
-}
-
-std::string triangle::kind() const
-{
-    if (kind_.empty()) {
         throw std::domain_error("Zero triangle");
     }
-    return kind_;
+    if (a < 0 || b < 0 || c < 0 || !triangle_equality(a, b, c)) {
+        return illegal;
+    }
+    if (a == b && b == c) {
+        return equilateral;
+    }
+    if (a == b || b == c || a == c) {
+        return isosceles;
+    }
+    return scalene;
+}
+
 }
