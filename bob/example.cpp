38a3284dfef8e8b107b2f6b1d1768d873419285f
Richard <legalize@xmission.com>
2014-08-10 00:42:14 -0600

    Remove vestiges of Unicode from earlier version

--- bob/example.cpp
+++ bob/example.cpp
@@ -10,26 +10,18 @@ namespace bob
 namespace
 {
 
-const char upper_case_a_umlaut = '\xc4';
-const char upper_case_u_umlaut = '\xdc';
-
 bool is_upper(string const& text)
 {
     auto last = end(text);
-    auto it = find_if_not(begin(text), last, [](char c) {
-        if (c < 0 || c > 127) {
-            return c == upper_case_a_umlaut
-                || c == upper_case_u_umlaut;
-        }
-        return c == toupper(c);
-    });
+    auto it = find_if_not(begin(text), last,
+        [](char c) { return c == toupper(c); });
     return it == last;
 }
 
 bool has_alpha(string const& text)
 {
     auto last = end(text);
-    auto it = find_if(begin(text), last, [](char c) { return c < 0 || c > 127 || isalpha(c); });
+    auto it = find_if(begin(text), last, [](char c) { return isalpha(c) != 0; });
     return it != last;
 }
 
@@ -37,7 +29,7 @@ string trim(string const& text)
 {
     auto first = text.find_first_not_of(' ');
     if (first == string::npos) {
-        return string();
+        return string{};
     }
     auto last = text.find_last_not_of(' ');
     return string(&text[first], &text[last]);
