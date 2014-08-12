22bea37fd82813c8a7241967eca94b1c6a69aeb2
Richard <legalize@xmission.com>
2014-08-11 23:06:16 -0600

    Pass raindrops

---
+++ raindrops/example.cpp
@@ -0,0 +1,30 @@
+#include "raindrops.h"
+#include <sstream>
+
+using namespace std;
+
+namespace raindrops
+{
+
+string convert(int drops)
+{
+    const int pling_factor{3};
+    const int plang_factor{5};
+    const int plong_factor{7};
+    ostringstream result;
+    if (drops % pling_factor == 0) {
+        result << "Pling";
+    }
+    if (drops % plang_factor == 0) {
+        result << "Plang";
+    }
+    if (drops % plong_factor == 0) {
+        result << "Plong";
+    }
+    if (!result.tellp()) {
+        result << drops;
+    }
+    return result.str();
+}
+
+}
---
+++ raindrops/raindrops_test.cpp
@@ -0,0 +1,83 @@
+#include "raindrops.h"
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(one_yields_itself)
+{
+    BOOST_REQUIRE_EQUAL("1", raindrops::convert(1));
+}
+
+BOOST_AUTO_TEST_CASE(three_yields_pling)
+{
+    BOOST_REQUIRE_EQUAL("Pling", raindrops::convert(3));
+}
+
+BOOST_AUTO_TEST_CASE(five_yields_plang)
+{
+    BOOST_REQUIRE_EQUAL("Plang", raindrops::convert(5));
+}
+
+BOOST_AUTO_TEST_CASE(seven_yields_plong)
+{
+    BOOST_REQUIRE_EQUAL("Plong", raindrops::convert(7));
+}
+
+BOOST_AUTO_TEST_CASE(six_yields_pling)
+{
+    BOOST_REQUIRE_EQUAL("Pling", raindrops::convert(6));
+}
+
+BOOST_AUTO_TEST_CASE(nine_yields_pling)
+{
+    BOOST_REQUIRE_EQUAL("Pling", raindrops::convert(9));
+}
+
+BOOST_AUTO_TEST_CASE(ten_yields_plang)
+{
+    BOOST_REQUIRE_EQUAL("Plang", raindrops::convert(10));
+}
+
+BOOST_AUTO_TEST_CASE(fourteen_yields_plong)
+{
+    BOOST_REQUIRE_EQUAL("Plong", raindrops::convert(14));
+}
+
+BOOST_AUTO_TEST_CASE(fifteen_yields_plingplang)
+{
+    BOOST_REQUIRE_EQUAL("PlingPlang", raindrops::convert(15));
+}
+
+BOOST_AUTO_TEST_CASE(twenty_one_yields_plingplong)
+{
+    BOOST_REQUIRE_EQUAL("PlingPlong", raindrops::convert(21));
+}
+
+BOOST_AUTO_TEST_CASE(twenty_five_yields_plang)
+{
+    BOOST_REQUIRE_EQUAL("Plang", raindrops::convert(25));
+}
+
+BOOST_AUTO_TEST_CASE(thirty_five_yields_plangplong)
+{
+    BOOST_REQUIRE_EQUAL("PlangPlong", raindrops::convert(35));
+}
+
+BOOST_AUTO_TEST_CASE(forty_nine_yields_plong)
+{
+    BOOST_REQUIRE_EQUAL("Plong", raindrops::convert(49));
+}
+
+BOOST_AUTO_TEST_CASE(fifty_two_yields_itself)
+{
+    BOOST_REQUIRE_EQUAL("52", raindrops::convert(52));
+}
+
+BOOST_AUTO_TEST_CASE(one_hundred_five_yields_plingplangplong)
+{
+    BOOST_REQUIRE_EQUAL("PlingPlangPlong", raindrops::convert(105));
+}
+
+BOOST_AUTO_TEST_CASE(big_prime_yields_itself)
+{
+    BOOST_REQUIRE_EQUAL("12121", raindrops::convert(12121));
+}
