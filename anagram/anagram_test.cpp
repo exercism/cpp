dce00207b68655ea3dedf860ec0c5b7814d7b108
Tim Lesgaux <tlesgaux@gmail.com>
2014-10-05 23:07:33 -0500

    Changed anagram::anagram to anagram::ana
    GCC was complaining that you can't call the constructor explicitly, so I changed it to call a static function `ana` which returns an instance of `anagram` (functionally the same as the previous commit)

--- anagram/anagram_test.cpp
+++ anagram/anagram_test.cpp
@@ -6,17 +6,17 @@ using namespace std;
 
 BOOST_AUTO_TEST_CASE(no_matches)
 {
-    auto subject = anagram::anagram("diaper");
+    auto subject = anagram::ana("diaper");
     auto matches = subject.matches({"hello", "world", "zombies", "pants"});
     vector<string> expected;
 
     BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
 }
-
 #if defined(EXERCISM_RUN_ALL_TESTS)
+
 BOOST_AUTO_TEST_CASE(detects_simple_anagram)
 {
-    auto subject = anagram::anagram("ant");
+    auto subject = anagram::ana("ant");
     auto matches = subject.matches({"tan", "stand", "at"});
     vector<string> expected{"tan"};
 
@@ -25,7 +25,7 @@ BOOST_AUTO_TEST_CASE(detects_simple_anagram)
 
 BOOST_AUTO_TEST_CASE(does_not_detect_false_positives)
 {
-    auto subject = anagram::anagram("galea");
+    auto subject = anagram::ana("galea");
     auto matches = subject.matches({"eagle"});
     vector<string> expected;
 
@@ -34,7 +34,7 @@ BOOST_AUTO_TEST_CASE(does_not_detect_false_positives)
 
 BOOST_AUTO_TEST_CASE(detects_multiple_anagrams)
 {
-    auto subject = anagram::anagram("master");
+    auto subject = anagram::ana("master");
     auto matches = subject.matches({"stream", "pigeon", "maters"});
     vector<string> expected{"stream", "maters"};
 
@@ -43,7 +43,7 @@ BOOST_AUTO_TEST_CASE(detects_multiple_anagrams)
 
 BOOST_AUTO_TEST_CASE(does_not_detect_anagram_subsets)
 {
-    auto subject = anagram::anagram("good");
+    auto subject = anagram::ana("good");
     auto matches = subject.matches({"dog", "goody"});
     vector<string> expected;
 
@@ -52,7 +52,7 @@ BOOST_AUTO_TEST_CASE(does_not_detect_anagram_subsets)
 
 BOOST_AUTO_TEST_CASE(detects_anagram)
 {
-    auto subject = anagram::anagram("listen");
+    auto subject = anagram::ana("listen");
     auto matches = subject.matches({"enlists", "google", "inlets", "banana"});
     vector<string> expected{"inlets"};
 
@@ -61,7 +61,7 @@ BOOST_AUTO_TEST_CASE(detects_anagram)
 
 BOOST_AUTO_TEST_CASE(detects_multiple_anagrams2)
 {
-    auto subject = anagram::anagram("allergy");
+    auto subject = anagram::ana("allergy");
     auto matches = subject.matches({"gallery", "ballerina", "regally", "clergy", "largely", "leading"});
     vector<string> expected{"gallery", "regally", "largely"};
 
@@ -70,7 +70,7 @@ BOOST_AUTO_TEST_CASE(detects_multiple_anagrams2)
 
 BOOST_AUTO_TEST_CASE(detects_anagrams_case_insensitively)
 {
-    auto subject = anagram::anagram("Orchestra");
+    auto subject = anagram::ana("Orchestra");
     auto matches = subject.matches({"cashregister", "Carthorse", "radishes"});
     vector<string> expected{"Carthorse"};
 
@@ -79,7 +79,7 @@ BOOST_AUTO_TEST_CASE(detects_anagrams_case_insensitively)
 
 BOOST_AUTO_TEST_CASE(does_not_detect_a_word_as_its_own_anagram)
 {
-    auto subject = anagram::anagram("banana");
+    auto subject = anagram::ana("banana");
     auto matches = subject.matches({"Banana"});
     vector<string> expected;
 
@@ -88,10 +88,11 @@ BOOST_AUTO_TEST_CASE(does_not_detect_a_word_as_its_own_anagram)
 
 BOOST_AUTO_TEST_CASE(matches_accepts_string_arguments)
 {
-    auto subject = anagram::anagram("ant");
+    auto subject = anagram::ana("ant");
     auto matches = subject.matches({"stand", "tan", "at"});
     vector<string> expected{"tan"};
 
     BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
 }
+
 #endif
