8fa1a6a8056e0c4128e1fcd9c9f8676afa2cac5b
Rob Prouse <rob@prouse.org>
2014-10-14 15:40:40 -0400

    Added unit tests that are in C#. The solution is less than ideal because I couldn't get boost::regex to compile under GCC on the Travis build machine.

--- word-count/example.cpp
+++ word-count/example.cpp
@@ -1,6 +1,12 @@
+#if defined(_MSC_VER) && _MSC_VER >= 1400 
+#pragma warning(push) 
+#pragma warning(disable:4996) 
+#endif 
+
 #include "word_count.h"
-#include <boost/algorithm/string/case_conv.hpp>
-#include <boost/tokenizer.hpp>
+#include <vector>
+#include <locale>
+#include <boost/algorithm/string.hpp>
 
 using namespace std;
 
@@ -9,11 +15,27 @@ namespace word_count
 
 map<string, int> words(string const& text)
 {
-    map<string, int> counts;
-    for (auto const& word : boost::tokenizer<>(text)) {
-        counts[boost::to_lower_copy(word)]++;
+    // A less than ideal solution since I can't get regex building in GCC on Travis
+    std::map<std::string, int> count;
+    std::string normalized = boost::to_lower_copy( text );
+    for ( unsigned int i = 0; i < normalized.length(); i++ )
+    {
+        auto c = normalized[i];
+        normalized[i] = ( std::isalnum( c, std::locale() ) || c == '\'' ) ? c : ' ';
     }
-    return counts;
+    std::vector<std::string> words;
+    boost::split( words, normalized, boost::is_any_of( "\t " ) );
+    for ( auto const& word : words )
+    {
+        auto const& trimmed = boost::trim_copy_if( word, boost::is_any_of( "' " ) );
+        if ( !trimmed.empty() )
+            count[trimmed]++;
+    }
+    return count;
 }
 
 }
+
+#if defined(_MSC_VER) && _MSC_VER >= 1400 
+#pragma warning(pop) 
+#endif 
