de9042f0a8562089bf50c15ae6a81303e9875658
Danny <liquidcore7@gmail.com>
2017-04-16 22:21:33 +0300

    queen-attack patch (#110)
    * Update queen_attack_test.cpp
    
    * Delete require_equal_containers.h
    
    * Update queen_attack_test.cpp

--- exercises/queen-attack/queen_attack_test.cpp
+++ exercises/queen-attack/queen_attack_test.cpp
@@ -1,14 +1,15 @@
 #include "queen_attack.h"
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
-#include "require_equal_containers.h"
 
 BOOST_AUTO_TEST_CASE(queens_in_default_positions)
 {
     const queen_attack::chess_board board;
 
-    BOOST_REQUIRE_EQUAL(std::make_pair(0, 3), board.white());
-    BOOST_REQUIRE_EQUAL(std::make_pair(7, 3), board.black());
+    BOOST_REQUIRE_EQUAL(std::make_pair(0, 3).first, board.white().first);
+    BOOST_REQUIRE_EQUAL(std::make_pair(0, 3).second, board.white().second);
+    BOOST_REQUIRE_EQUAL(std::make_pair(7, 3).first, board.black().first);
+    BOOST_REQUIRE_EQUAL(std::make_pair(7, 3).second, board.black().second);
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
@@ -18,8 +19,10 @@ BOOST_AUTO_TEST_CASE(initialized_with_specific_positions)
     const auto black = std::make_pair(6, 1);
     const queen_attack::chess_board board{white, black};
 
-    BOOST_REQUIRE_EQUAL(white, board.white());
-    BOOST_REQUIRE_EQUAL(black, board.black());
+    BOOST_REQUIRE_EQUAL(white.first, board.white().first);
+    BOOST_REQUIRE_EQUAL(white.second, board.white().second);
+    BOOST_REQUIRE_EQUAL(black.first, board.black().first);
+    BOOST_REQUIRE_EQUAL(black.second, board.black().second);
 }
 
 BOOST_AUTO_TEST_CASE(queen_positions_must_be_distinct)
