444d41277a53ddb721df411216cf6a9463ea8b14
Patrick Jackson <PatrickSJackson@gmail.com>
2017-04-16 13:06:51 -0700

    word-count: Removing require_equal_containers.h

--- exercises/word-count/word_count_test.cpp
+++ exercises/word-count/word_count_test.cpp
@@ -2,7 +2,6 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 #include <map>
-#include "require_equal_containers.h"
 
 using namespace std;
 
@@ -12,7 +11,7 @@ BOOST_AUTO_TEST_CASE(counts_one_word)
 
     const auto actual = word_count::words("word");
 
-    REQUIRE_EQUAL_CONTAINERS(expected, actual);
+    BOOST_TEST(expected == actual);
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
@@ -22,7 +21,7 @@ BOOST_AUTO_TEST_CASE(counts_one_of_each)
 
     const auto actual = word_count::words("one of each");
 
-    REQUIRE_EQUAL_CONTAINERS(expected, actual);
+    BOOST_TEST(expected == actual);
 }
 
 BOOST_AUTO_TEST_CASE(counts_multiple_occurrences)
@@ -31,7 +30,7 @@ BOOST_AUTO_TEST_CASE(counts_multiple_occurrences)
 
     const auto actual = word_count::words("one fish two fish red fish blue fish");
 
-    REQUIRE_EQUAL_CONTAINERS(expected, actual);
+    BOOST_TEST(expected == actual);
 }
 
 BOOST_AUTO_TEST_CASE(ignores_punctuation)
@@ -40,7 +39,7 @@ BOOST_AUTO_TEST_CASE(ignores_punctuation)
 
     const auto actual = word_count::words("car : carpet as java : javascript!!&@$%^&");
 
-    REQUIRE_EQUAL_CONTAINERS(expected, actual);
+    BOOST_TEST(expected == actual);
 }
 
 BOOST_AUTO_TEST_CASE(includes_numbers)
@@ -49,7 +48,7 @@ BOOST_AUTO_TEST_CASE(includes_numbers)
 
     const auto actual = word_count::words("testing, 1, 2 testing");
 
-    REQUIRE_EQUAL_CONTAINERS(expected, actual);
+    BOOST_TEST(expected == actual);
 }
 
 BOOST_AUTO_TEST_CASE(normalizes_case)
@@ -58,7 +57,7 @@ BOOST_AUTO_TEST_CASE(normalizes_case)
 
     const auto actual = word_count::words("go Go GO");
 
-    REQUIRE_EQUAL_CONTAINERS(expected, actual);
+    BOOST_TEST(expected == actual);
 }
 
 BOOST_AUTO_TEST_CASE(counts_constructor)
@@ -67,7 +66,7 @@ BOOST_AUTO_TEST_CASE(counts_constructor)
 
     const auto actual = word_count::words("constructor Constructor");
 
-    REQUIRE_EQUAL_CONTAINERS(expected, actual);
+    BOOST_TEST(expected == actual);
 }
 
 BOOST_AUTO_TEST_CASE(counts_multiline)
@@ -76,15 +75,15 @@ BOOST_AUTO_TEST_CASE(counts_multiline)
 
     const auto actual = word_count::words("hello\nworld");
 
-    REQUIRE_EQUAL_CONTAINERS(expected, actual);
+    BOOST_TEST(expected == actual);
 }
 
 BOOST_AUTO_TEST_CASE(count_everything_just_once)
 {
     const map<string, int> expected{{"all", 2}, {"the", 2}, {"kings", 2}, {"horses", 1}, {"and", 1}, {"men", 1}};
     const auto actual = word_count::words("all the kings horses and all the kings men");
-    
-    REQUIRE_EQUAL_CONTAINERS(expected, actual);
+
+    BOOST_TEST(expected == actual);
 }
 
 BOOST_AUTO_TEST_CASE(handles_cramped_list)
@@ -92,7 +91,7 @@ BOOST_AUTO_TEST_CASE(handles_cramped_list)
     const map<string, int> expected{{"one", 1}, {"two", 1}, {"three", 1}};
     const auto actual = word_count::words("one,two,three");
 
-    REQUIRE_EQUAL_CONTAINERS(expected, actual);
+    BOOST_TEST(expected == actual);
 }
 
 BOOST_AUTO_TEST_CASE(with_apostrophes)
@@ -100,7 +99,7 @@ BOOST_AUTO_TEST_CASE(with_apostrophes)
     const map<string, int> expected{{"first", 1}, {"don't", 2}, {"laugh", 1}, {"then", 1}, {"cry", 1}};
     const auto actual = word_count::words("First: don't laugh. Then: don't cry.");
 
-    REQUIRE_EQUAL_CONTAINERS(expected, actual);
+    BOOST_TEST(expected == actual);
 }
 
 BOOST_AUTO_TEST_CASE(with_free_standing_apostrophes)
@@ -108,7 +107,7 @@ BOOST_AUTO_TEST_CASE(with_free_standing_apostrophes)
     const map<string, int> expected{{ "go", 3 }};
     const auto actual = word_count::words("go ' Go '' GO");
 
-    REQUIRE_EQUAL_CONTAINERS(expected, actual);
+    BOOST_TEST(expected == actual);
 }
 
 BOOST_AUTO_TEST_CASE(with_apostrophes_as_quotes)
@@ -116,6 +115,6 @@ BOOST_AUTO_TEST_CASE(with_apostrophes_as_quotes)
     const map<string, int> expected{{"she", 1}, {"said", 1}, {"let's", 1}, {"meet", 1}, {"at", 1}, {"twelve", 1}, {"o'clock", 1}};
     const auto actual = word_count::words("She said, 'let's meet at twelve o'clock'");
 
-    REQUIRE_EQUAL_CONTAINERS(expected, actual);
+    BOOST_TEST(expected == actual);
 }
 #endif
