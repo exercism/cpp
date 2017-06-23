4780874bcae358427b04443bc486f9d52449ac16
vivshaw <hvivianshaw@gmail.com>
2017-06-23 00:01:06 -0400

    implement first compound allergy test & fix tabs on previous tests

--- exercises/allergies/allergies_test.cpp
+++ exercises/allergies/allergies_test.cpp
@@ -13,58 +13,67 @@ BOOST_AUTO_TEST_CASE(has_no_allergies)
 
 BOOST_AUTO_TEST_CASE(allergic_to_eggs)
 {
-   allergies::allergy_test test(1);
+    allergies::allergy_test test(1);
    
-   BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("eggs"));
+    BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("eggs"));
 }
 
 BOOST_AUTO_TEST_CASE(allergic_to_peanuts)
 {
-   allergies::allergy_test test(2);
+    allergies::allergy_test test(2);
    
-   BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("peanuts"));
+    BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("peanuts"));
 }
 
 BOOST_AUTO_TEST_CASE(allergic_to_shellfish)
 {
-   allergies::allergy_test test(4);
+    allergies::allergy_test test(4);
    
-   BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("shellfish"));
+    BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("shellfish"));
 }
 
 BOOST_AUTO_TEST_CASE(allergic_to_strawberries)
 {
-   allergies::allergy_test test(8);
+    allergies::allergy_test test(8);
    
-   BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("strawberries"));
+    BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("strawberries"));
 }
 
 BOOST_AUTO_TEST_CASE(allergic_to_tomatoes)
 {
-   allergies::allergy_test test(16);
+    allergies::allergy_test test(16);
    
-   BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("tomatoes"));
+    BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("tomatoes"));
 }
 
 BOOST_AUTO_TEST_CASE(allergic_to_chocolate)
 {
-   allergies::allergy_test test(32);
+    allergies::allergy_test test(32);
    
-   BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("chocolate"));
+    BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("chocolate"));
 }
 
 BOOST_AUTO_TEST_CASE(allergic_to_pollen)
 {
-   allergies::allergy_test test(64);
+    allergies::allergy_test test(64);
    
-   BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("pollen"));
+    BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("pollen"));
 }
 
 BOOST_AUTO_TEST_CASE(allergic_to_cats)
 {
-   allergies::allergy_test test(128);
+    allergies::allergy_test test(128);
    
-   BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("cats"));
+    BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("cats"));
+}
+
+BOOST_AUTO_TEST_CASE(allergic_to_eggs_and_other_stuff)
+{
+    allergies::allergy_test test(5);
+   
+    BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("eggs"));
+    BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("shellfish"));
+    BOOST_REQUIRE_EQUAL(false, test.is_allergic_to("peanuts"));
 }
 
 
