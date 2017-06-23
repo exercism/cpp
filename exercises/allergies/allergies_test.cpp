289360b2b71c92a5d1267fe6806b3562b78fd4ad
vivshaw <hvivianshaw@gmail.com>
2017-06-22 23:40:58 -0400

    fix const errors in first two tests

--- exercises/allergies/allergies_test.cpp
+++ exercises/allergies/allergies_test.cpp
@@ -1,22 +1,21 @@
 #include "allergies.h"
-#include <stdexcept>
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(has_no_allergies)
 {
-    const allergies::allergy_test test(0);
+    allergies::allergy_test test(0);
     
-    BOOST_REQUIRE_EQUAL(False, test.is_allergic_to("Peanuts"));
-    BOOST_REQUIRE_EQUAL(False, test.is_allergic_to("Cats"));
-    BOOST_REQUIRE_EQUAL(False, test.is_allergic_to("Strawberries"));
+    BOOST_REQUIRE_EQUAL(false, test.is_allergic_to("peanuts"));
+    BOOST_REQUIRE_EQUAL(false, test.is_allergic_to("cats"));
+    BOOST_REQUIRE_EQUAL(false, test.is_allergic_to("strawberries"));
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(allergic_to_eggs)
 {
-   const allergies::allergy_test test(1);
+   allergies::allergy_test test(1);
    
-   BOOST_REQUIRE_EQUAL(True, test.is_allergic_to("Strawberries"));
+   BOOST_REQUIRE_EQUAL(true, test.is_allergic_to("eggs"));
 }
+#if defined(EXERCISM_RUN_ALL_TESTS)
 #endif
