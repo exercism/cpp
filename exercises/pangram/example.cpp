4ffde64dfdf82378839a04987efdd34e4cbdbbec
Is0metry <sims.woodrow@gmail.com>
2017-09-10 15:28:10 -0500

    Implementing pangram. (#148)
    * Implementing pangram.
    
    * completed implementation of pangram.
    
    * forgot the config
    
    * enforce repo style
    
    * further enforced style conventions.
    
    * forgot a semicolon.
    
    * updated config.json (sorry about that).
    
    * changed function input to const reference.

---
+++ exercises/pangram/example.cpp
@@ -0,0 +1,35 @@
+#include "pangram.h"
+#include <cctype>
+
+namespace
+{
+    
+bool found_letter(char a, const std::string &s)
+{
+    for(auto c: s) {
+        if(tolower(c) ==a) {
+            return true;
+        }
+    }
+    return false;
+}
+    
+}
+
+namespace pangram
+{
+    
+bool is_pangram(const std::string &s)
+{
+    char letters[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    for(char l : letters)
+    {
+        if(!found_letter(l, s))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+}
