1dd40f333f4cc88fcae5b839a524405eddb41a6d
Smanikonda <susie.manikonda@gmail.com>
2018-04-04 16:43:42 -0400

    create example.h

---
+++ exercises/protein-translation/example.h
@@ -0,0 +1,10 @@
+#if !defined(PROTEIN_TRANSLATION_H)
+#define PROTEIN_TRANSLATION_H
+
+#include <string>
+
+namespace protein_translation
+{
+   void protein_translation(std::string rna);
+}
+#endif
