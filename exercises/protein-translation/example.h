3996a2a8d9a189a3828af77413e5895f678c0b8d
Smanikonda <susie.manikonda@gmail.com>
2018-04-09 22:53:57 -0400

    add exercise - protein-translation

--- exercises/protein-translation/example.h
+++ exercises/protein-translation/example.h
@@ -2,9 +2,11 @@
 #define PROTEIN_TRANSLATION_H
 
 #include <string>
+#include <vector>
 
 namespace protein_translation
 {
-   void protein_translation(std::string rna);
+   std::vector<std::string> protein_translation(std::string rna);
+   bool isProtein(std::vector<std::string> arg, std::string rna);
 }
 #endif
