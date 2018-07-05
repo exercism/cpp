44ab23ce8ead9c45ac8496abb2eb50bf7ce3841c
Sahil Tara <supertfriend@gmail.com>
2018-07-05 18:49:07 -0400

    Added Isogram exercise

---
+++ exercises/isogram/example.cpp
@@ -0,0 +1,27 @@
+#include "isogram.h"
+
+#include <string>
+#include <unordered_set>
+
+bool isogram::is_isogram(std::string word) {
+	std::unordered_set<char> letters;
+	const std::unordered_set<char> alphabet({'a','b','c','d','e','f','g','h',
+		                                           'i','j','k','l','m','n','o','p',
+												   'q','r','s','t','u','v','w','x',
+		                                           'y','z'});
+
+	for (auto&& ch : word) {
+		ch = tolower(ch);
+
+		// character not parsed over
+		if (letters.find(ch) == letters.end()) {
+			letters.insert(ch);
+		}
+		// character is a member of the alphabet
+		else if (alphabet.find(ch) != alphabet.end()) {
+			return false;
+		}
+	}
+	
+	return true;
+}
---
+++ exercises/isogram/example.h
@@ -0,0 +1,10 @@
+#ifndef ISOGRAM_H
+#define ISOGRAM_H
+
+#include <string>
+namespace isogram {
+	bool is_isogram(std::string word);
+}
+
+#endif
+
