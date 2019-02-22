101ce4a0852d8f781fd81a5ebe57aa8f1c7c9ea5
Elyashiv <>
2019-02-22 15:36:23 +0200

    finished basic tests

--- exercises/binary_search_tree/binary_search_tree_test.cpp
+++ exercises/binary_search_tree/binary_search_tree_test.cpp
@@ -61,10 +61,28 @@ BOOST_AUTO_TEST_CASE(same_number_at_left_node)
     test_leaf(tested->left(), 4, false, false);
 }
 
-// BOOST_AUTO_TEST_CASE(greater_number_at_right_node)
-// {
-//     auto 
-// }
+BOOST_AUTO_TEST_CASE(greater_number_at_right_node)
+{
+    auto tested = make_tree({4, 5});
+    
+    test_leaf(tested, 4, false, true);
+    test_leaf(tested->right(), 5, false, false);
+}
+
+BOOST_AUTO_TEST_CASE(can_create_complex_tree)
+{
+    auto tested = make_tree({4, 2, 6, 1, 3, 5, 7});
+
+    test_leaf(tested, 4, true, true);
+    test_leaf(tested->left(), 2, true, true);
+    test_leaf(tested->right(), 6, true, true);
+
+    test_leaf(tested->left()->left(), 1, false, false);
+    test_leaf(tested->left()->right(), 3, false, false);
+
+    test_leaf(tested->right()->left(), 5, false, false);
+    test_leaf(tested->right()->right(), 7, false, false);
+}
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
 #endif
