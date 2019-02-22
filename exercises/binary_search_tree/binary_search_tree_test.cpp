6164db6cf111d98d8aaef8390d361e5832d0efee
Elyashiv <>
2019-02-22 15:22:42 +0200

    another test case

--- exercises/binary_search_tree/binary_search_tree_test.cpp
+++ exercises/binary_search_tree/binary_search_tree_test.cpp
@@ -39,5 +39,14 @@ BOOST_AUTO_TEST_CASE(insert_data_at_proper_node)
     test_leaf(tested->left(), 2, false, false);
 }
 
+BOOST_AUTO_TEST_CASE(same_number_at_left_node)
+{
+    auto tested = make_tree(4);
+    tested->insert(4);
+
+    test_leaf(tested, 4, true, false);
+    test_leaf(tested->left(), 4, false, false);
+}
+
 #if defined(EXERCISM_RUN_ALL_TESTS)
 #endif
