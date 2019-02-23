41447468131df78db4f76a1989c1eec41c130546
Elyashiv <>
2019-02-23 20:39:59 +0200

    started impelemnting an iterator

--- exercises/binary_search_tree/binary_search_tree.cpp
+++ exercises/binary_search_tree/binary_search_tree.cpp
@@ -1,14 +1,17 @@
 #include "binary_search_tree.h"
+#include <exception>
 
 namespace binary_tree
 {
+    // tree
+
     binary_tree::binary_tree(const tree_data_t data)
     : _data(data),
       _left(nullptr),
       _right(nullptr)
     {}
 
-    binary_tree::tree_data_t binary_tree::data() const
+    const binary_tree::tree_data_t &binary_tree::data() const
     {
         return _data;
     }
@@ -35,5 +38,117 @@ namespace binary_tree
     {
         binary_tree_ptr &insert_location = data > _data ? _right : _left;
         insert(data, insert_location);
+    }
+
+    // iterator
+
+    binary_tree::binary_tree_iter::binary_tree_iter(const binary_tree_ptr &tree)
+    : _tree(tree),
+      _side_iter(build_first_side_iter(tree))
+    {
+        _state = _tree->left() ? state::Left : state::Middle;
+    }
+
+    binary_tree::binary_tree_iter::binary_tree_iter_ptr binary_tree::binary_tree_iter::build_first_side_iter(const binary_tree_ptr &tree)
+    {
+        if (!tree->left())
+            return nullptr;
+        return binary_tree_iter_ptr(new binary_tree_iter(tree->left()));
+    }
+
+    binary_tree::binary_tree_iter binary_tree::binary_tree_iter::build_end_iterator(const binary_tree_ptr &tree)
+    {
+        binary_tree_iter iter(tree);
+        iter._side_iter.reset(nullptr);
+        iter._state = state::Done;
+
+        return iter;
+    }
+
+    // binary_tree::binary_tree_iter::binary_tree_iter(binary_tree_iter &other)
+    // : _tree(other._tree),
+    //   _state(other._state),
+    //   _side_iter()
+    // {
+    // }
+
+    // binary_tree::binary_tree_iter::binary_tree_iter_ptr binary_tree::binary_tree_iter::copy_side_iter(const binary_tree::binary_tree_iter &other)
+    // {
+    //     if (!other._side_iter)
+    //         return nullptr;
+    //     return binary_tree_iter_ptr(new binary_tree_iter(*other._side_iter);
+    // }
+
+    bool binary_tree::binary_tree_iter::operator==(const binary_tree::binary_tree_iter &other) const
+    {
+        if (_tree != other._tree)
+            return false;
+        if (_state != other._state)
+            return false;
+        return _side_iter == other._side_iter;
+    }
+
+    bool binary_tree::binary_tree_iter::operator!=(const binary_tree_iter &other) const
+    {
+        return !(*this == other);
+    }
+
+    const binary_tree::tree_data_t& binary_tree::binary_tree_iter::operator*() const
+    {
+        switch (_state)
+        {
+        case state::Done:
+            throw std::out_of_range("Access of iterator after end");
+        case state::Middle:
+            return _tree->data();
+        case state::Left:
+        case state::Right:
+            return _side_iter->operator*();
+        default:
+            throw std::logic_error("Missing switch value");
+        }
+    }
+
+    const binary_tree::tree_data_t* binary_tree::binary_tree_iter::operator->() const
+    {
+        return &this->operator*();
+    }
+
+    binary_tree::binary_tree_iter& binary_tree::binary_tree_iter::operator++()
+    {
+        switch (_state)
+        {
+        case state::Done:
+            throw std::out_of_range("Cannot advance iterator after end");
+        case state::Middle:
+            if (!_tree->right())
+            {
+                _state = state::Done;
+                break;
+            }
+            _state = state::Right;
+            _side_iter = binary_tree_iter_ptr(new binary_tree_iter(_tree->right()));
+            break;
+        case state::Left:
+            advance_side_iter(state::Middle);
+            break;
+        case state::Right:
+            advance_side_iter(state::Done);
+            break;
+        default:
+            throw std::logic_error("Missing switch value");
+        }
+
+        return *this;
+    }
+
+    void binary_tree::binary_tree_iter::advance_side_iter(state next_state)
+    {
+        _side_iter->operator++();
+        if (_side_iter->_state == state::Done)
+        {
+            _side_iter.reset(nullptr);
+            _state = next_state;
+        }
     }
 }
--- exercises/binary_search_tree/binary_search_tree.h
+++ exercises/binary_search_tree/binary_search_tree.h
@@ -16,14 +16,14 @@ namespace binary_tree
         ~binary_tree() = default;
 
         binary_tree(binary_tree &) = delete;
-        binary_tree &operator=(binary_tree &) = delete;
+        binary_tree& operator=(binary_tree &) = delete;
 
         binary_tree(binary_tree &&) = delete;
-        binary_tree &&operator=(binary_tree &&) = delete;
+        binary_tree&& operator=(binary_tree &&) = delete;
 
         void insert(tree_data_t data);
 
-        tree_data_t data() const;
+        const tree_data_t &data() const;
         const binary_tree_ptr& left() const;
         const binary_tree_ptr& right() const;
 
@@ -33,6 +33,49 @@ namespace binary_tree
         tree_data_t _data;
         binary_tree_ptr _left;
         binary_tree_ptr _right;
+
+    public:
+        class binary_tree_iter final
+        {
+        public:
+            using binary_tree_iter_ptr = std::unique_ptr<binary_tree_iter>;
+        private:
+            enum class state
+            {
+                Left,
+                Right,
+                Middle,
+                Done
+            };
+
+            const binary_tree_ptr &_tree;
+            state _state;
+            binary_tree_iter_ptr _side_iter;
+
+            explicit binary_tree_iter(const binary_tree_ptr &tree);
+            static binary_tree_iter build_end_iterator(const binary_tree_ptr &tree);
+
+        public:
+            ~binary_tree_iter() = default;
+
+            binary_tree_iter(binary_tree_iter &) = delete;
+            binary_tree_iter & operator=(binary_tree_iter &) = delete;
+
+            binary_tree_iter(binary_tree_iter &&) = default;
+            binary_tree_iter & operator=(binary_tree_iter &&) = default;
+
+            const tree_data_t& operator*() const;
+            const tree_data_t* operator->() const;
+            binary_tree_iter& operator++();
+
+            bool operator==(const binary_tree_iter &) const;
+            bool operator!=(const binary_tree_iter &) const;
+        
+        private:
+            void advance_side_iter(state next_state);
+            static binary_tree_iter_ptr build_first_side_iter(const binary_tree_ptr &tree);
+            static binary_tree_iter_ptr copy_side_iter(const binary_tree_iter &other);
+        };
     };
 }
 
