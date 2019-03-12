a4ee967b0a09d4af2d64403a05af237b3ed7078b
chgraef <1595191+chgraef@users.noreply.github.com>
2019-03-12 22:47:34 +0100

    Tidy up for coherent look

--- exercises/robot-simulator/example.cpp
+++ exercises/robot-simulator/example.cpp
@@ -1,13 +1,14 @@
 #include "robot_simulator.h"
 
-namespace robot_simulator
-{
+namespace robot_simulator {
 
 Robot::Robot() : robot_position(std::make_pair(0, 0)), robot_bearing(Bearing::NORTH) 
 {
 }
 
-Robot::Robot(std::pair<int, int> p, Bearing b) : robot_position(p), robot_bearing(b) {}
+Robot::Robot(std::pair<int, int> p, Bearing b) : robot_position(p), robot_bearing(b) 
+{
+}
 
 Bearing Robot::get_bearing() const
 {
@@ -48,16 +49,24 @@ void Robot::turn_left()
     switch(robot_bearing)
     {
         case Bearing::NORTH:
+        {
             robot_bearing = Bearing::WEST;
             break;
+        }
         case Bearing::WEST:
+        {
             robot_bearing = Bearing::SOUTH;
             break;
+        }
         case Bearing::SOUTH:
+        {
             robot_bearing = Bearing::EAST;
             break;
+        }
         case Bearing::EAST:
+        {
             robot_bearing = Bearing::NORTH;
+        }
     }
 }
 
@@ -66,16 +75,24 @@ void Robot::advance()
     switch(robot_bearing)
     {
         case Bearing::NORTH:
+        {
             robot_position.second++;
             break;
+        }
         case Bearing::WEST:
+        {
             robot_position.first--;
             break;
+        }
         case Bearing::SOUTH:
+        {
             robot_position.second--;
             break;
+        }
         case Bearing::EAST:
+        {
             robot_position.first++;
+        }
     } 
 }
 
@@ -86,13 +103,19 @@ void Robot::execute_sequence(std::string const& sequence)
         switch(c)
         {
             case 'L':
+            {
                 turn_left();
                 break;
+            }
             case 'R':
+            {
                 turn_right();
                 break;
+            }
             case 'A':
+            {
                 advance();
+            }
         }
     }
 }
