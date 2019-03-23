f6a9eb5d22587ca1efaad09b3be5eb6b635c77ba
elyashiv <elyashi.horovitz@gmail.com>
2019-03-23 18:35:51 +0200

    Implement exercise: binary search tree (#220)
    * moved to use enum class
    
    * just a small start
    
    * another test case
    
    * refactor test code
    
    * actually use teh refactor
    
    * finished basic tests
    
    * a few modifiers
    
    * renamed a test
    
    * started impelemnting an iterator
    
    * finished iterator impl
    
    * added sort test cases
    
    * fixed conventions
    
    * added to the repo
    
    * Revert "moved to use enum class"
    
    This reverts commit 1c8522bf55369c0715ad434c1c8ab9b5b8eebddf.
    
    * changed check of the nullptr
    
    * fixed clang warning
    
    * moved to use generics
    
    * added generic tests
    
    * removed unneded dependencies
    
    * higher dificulty
    
    * small fixup of tests
    
    * removed oneline function
    
    * added iterator comment
    
    * removed unused function
    
    * renamed consts to use uppercase
    
    * moved membvers definition
    
    * side_iter -> branch_iter
    
    * remove unused private method
    
    * use strings for string tests
    
    * use proper move
    
    * Adding classes as a topic.

---
+++ exercises/binary-search-tree/binary_search_tree_test.cpp
@@ -0,0 +1,142 @@
+#include "binary_search_tree.h"
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+#include <vector>
+
+// test data version: 1.0.0
+
+template<typename T>
+using tree_ptr = typename std::unique_ptr<binary_tree::binary_tree<T>>;
+
+template<typename T>
+static void test_leaf(const tree_ptr<T> &tree, 
+                      const T& data, bool has_left, bool has_right)
+{
+    BOOST_REQUIRE_EQUAL(data, tree->data());
+    BOOST_REQUIRE_EQUAL((bool) tree->left(), has_left);
+    BOOST_REQUIRE_EQUAL((bool) tree->right(), has_right);
+}
+
+template<typename T>
+static tree_ptr<T> make_tree(const std::vector<T> &data)
+{
+    if (data.empty())
+        return tree_ptr<T>(nullptr);
+    
+    auto data_iter = data.begin();
+    auto tree = tree_ptr<T>(new binary_tree::binary_tree<T>(*data_iter));
+    ++data_iter;
+
+    for (; data_iter != data.end(); ++data_iter)
+    {
+        tree->insert(*data_iter);
+    }
+
+    return tree;
+}
+
+BOOST_AUTO_TEST_CASE(data_is_retained)
+{
+    auto tested = make_tree<uint32_t>({4});
+    test_leaf<uint32_t>(tested, 4, false, false);
+}
+
+#if defined(EXERCISM_RUN_ALL_TESTS)
+
+BOOST_AUTO_TEST_CASE(smaller_number_at_left_node)
+{
+    auto tested = make_tree<uint32_t>({4, 2});
+
+    test_leaf<uint32_t>(tested, 4, true, false);
+    test_leaf<uint32_t>(tested->left(), 2, false, false);
+}
+
+BOOST_AUTO_TEST_CASE(same_number_at_left_node)
+{
+    auto tested = make_tree<uint32_t>({4, 4});
+
+    test_leaf<uint32_t>(tested, 4, true, false);
+    test_leaf<uint32_t>(tested->left(), 4, false, false);
+}
+
+BOOST_AUTO_TEST_CASE(greater_number_at_right_node)
+{
+    auto tested = make_tree<uint32_t>({4, 5});
+    
+    test_leaf<uint32_t>(tested, 4, false, true);
+    test_leaf<uint32_t>(tested->right(), 5, false, false);
+}
+
+BOOST_AUTO_TEST_CASE(can_create_complex_tree)
+{
+    auto tested = make_tree<uint32_t>({4, 2, 6, 1, 3, 5, 7});
+
+    test_leaf<uint32_t>(tested, 4, true, true);
+    test_leaf<uint32_t>(tested->left(), 2, true, true);
+    test_leaf<uint32_t>(tested->right(), 6, true, true);
+
+    test_leaf<uint32_t>(tested->left()->left(), 1, false, false);
+    test_leaf<uint32_t>(tested->left()->right(), 3, false, false);
+
+    test_leaf<uint32_t>(tested->right()->left(), 5, false, false);
+    test_leaf<uint32_t>(tested->right()->right(), 7, false, false);
+}
+
+// The tests below require an implementation of an iterator.
+// You can get more details here: http://www.cplusplus.com/reference/iterator/
+
+template<typename T>
+static void test_sort(const tree_ptr<T> &tree, const std::vector<T> &expected)
+{
+    BOOST_REQUIRE_EQUAL_COLLECTIONS(tree->begin(), tree->end(), expected.begin(), expected.end());
+}
+
+
+BOOST_AUTO_TEST_CASE(can_sort_single_number)
+{
+    test_sort(make_tree<uint32_t>({2}), {2});
+}
+
+BOOST_AUTO_TEST_CASE(can_sort_if_second_number_is_smaller_than_first)
+{
+    test_sort(make_tree<uint32_t>({2, 1}), {1, 2});
+}
+
+BOOST_AUTO_TEST_CASE(can_sort_if_second_number_is_same_as_first)
+{
+    test_sort(make_tree<uint32_t>({2, 2}), {2, 2});
+}
+
+BOOST_AUTO_TEST_CASE(can_sort_if_second_number_is_greater_than_first)
+{
+    test_sort(make_tree<uint32_t>({2, 3}), {2, 3});
+}
+
+BOOST_AUTO_TEST_CASE(can_sort_complex_tree)
+{
+    test_sort(make_tree<uint32_t>({2, 1, 3, 6, 7, 5}), {1, 2, 3, 5, 6, 7});
+}
+
+// strings
+
+BOOST_AUTO_TEST_CASE(can_create_complex_tree_strings)
+{
+    auto tested = make_tree<std::string>({"delicious", "ballon", "flag", "apple", "cat", "elispsis", "grains"});
+
+    test_leaf<std::string>(tested, "delicious", true, true);
+    test_leaf<std::string>(tested->left(), "ballon", true, true);
+    test_leaf<std::string>(tested->right(), "flag", true, true);
+
+    test_leaf<std::string>(tested->left()->left(), "apple", false, false);
+    test_leaf<std::string>(tested->left()->right(), "cat", false, false);
+
+    test_leaf<std::string>(tested->right()->left(), "elispsis", false, false);
+    test_leaf<std::string>(tested->right()->right(), "grains", false, false);
+}
+
+BOOST_AUTO_TEST_CASE(can_sort_complex_tree_strings)
+{
+    test_sort(make_tree<std::string>({"A", "few", "random", "strings", "that", "should", "be", "sorted"}), {"A", "be", "few", "random", "should", "sorted", "strings", "that"});
+}
+
+#endif
---
+++ exercises/binary-search-tree/example.h
@@ -0,0 +1,220 @@
+#if !defined(_BINARY_SEARCH_TREE_H_)
+#define _BINARY_SEARCH_TREE_H_
+
+#include <memory>
+#include <cstdint>
+
+namespace binary_tree
+{
+    using std::move;
+    template<typename T>
+    class binary_tree final
+    {
+    public:
+        class binary_tree_iter;
+        using binary_tree_ptr = std::unique_ptr<binary_tree>;
+
+        explicit binary_tree(T data)
+            : _data(move(data)),
+              _left(nullptr),
+              _right(nullptr)
+        {}
+
+        ~binary_tree() = default;
+
+        binary_tree(binary_tree &) = delete;
+        binary_tree& operator=(binary_tree &) = delete;
+
+        binary_tree(binary_tree &&) = delete;
+        binary_tree&& operator=(binary_tree &&) = delete;
+
+    public:
+        void insert(const T &data);
+
+        const T &data() const {return _data;};
+        const binary_tree_ptr& left() const {return _left;};
+        const binary_tree_ptr& right() const {return _right;};
+
+        binary_tree_iter begin() const;
+        binary_tree_iter end() const;
+        
+    private:
+        T _data;
+        binary_tree_ptr _left;
+        binary_tree_ptr _right;
+
+    public:
+        class binary_tree_iter final
+        {
+            friend binary_tree_iter binary_tree::begin() const;
+            friend binary_tree_iter binary_tree::end() const;
+        public:
+            using binary_tree_iter_ptr = std::unique_ptr<binary_tree_iter>;
+        private:
+            enum class state
+            {
+                LEFT,
+                RIGHT,
+                MIDDLE,
+                DONE
+            };
+
+            explicit binary_tree_iter(const binary_tree &tree);
+            static binary_tree_iter build_end_iterator(const binary_tree &tree);
+
+        public:
+            ~binary_tree_iter() = default;
+
+            binary_tree_iter(binary_tree_iter &) = delete;
+            binary_tree_iter & operator=(binary_tree_iter &) = delete;
+
+            binary_tree_iter(binary_tree_iter &&) = default;
+            binary_tree_iter & operator=(binary_tree_iter &&) = default;
+
+            const T& operator*() const;
+            const T* operator->() const;
+            binary_tree_iter& operator++();
+
+            bool operator==(const binary_tree_iter &) const;
+            bool operator!=(const binary_tree_iter &) const;
+        
+        private:
+            void advance_branch_iter(state next_state);
+            static binary_tree_iter_ptr build_first_branch_iter(const binary_tree &tree);
+            static binary_tree_iter_ptr copy_branch_iter(const binary_tree_iter &other);
+
+            const binary_tree &_tree;
+            state _state;
+            binary_tree_iter_ptr _branch_iter;
+        };
+    };
+
+    // tree
+
+    template<typename T>
+    void binary_tree<T>::insert(const T &data)
+    {
+        binary_tree_ptr &insert_location = data > _data ? _right : _left;
+        if (!insert_location)
+            insert_location = binary_tree_ptr(new binary_tree(data));
+        else
+            insert_location->insert(data);   
+    }
+
+    template<typename T>
+    typename binary_tree<T>::binary_tree_iter binary_tree<T>::begin() const {return binary_tree_iter(*this);}
+    template<typename T>
+    typename binary_tree<T>::binary_tree_iter binary_tree<T>::end() const {return binary_tree_iter::build_end_iterator(*this);}
+
+
+    // iter
+    template<typename T>
+    binary_tree<T>::binary_tree_iter::binary_tree_iter(const binary_tree<T> &tree)
+    : _tree(tree),
+      _branch_iter(build_first_branch_iter(tree))
+    {
+        _state = _tree.left() ? state::LEFT : state::MIDDLE;
+    }
+
+    template<typename T>
+    typename binary_tree<T>::binary_tree_iter::binary_tree_iter_ptr binary_tree<T>::binary_tree_iter::build_first_branch_iter(const binary_tree &tree)
+    {
+        if (!tree.left())
+            return nullptr;
+        return binary_tree_iter_ptr(new binary_tree_iter(*tree.left()));
+    }
+
+
+    template<typename T>
+    typename binary_tree<T>::binary_tree_iter binary_tree<T>::binary_tree_iter::build_end_iterator(const binary_tree<T> &tree)
+    {
+        binary_tree_iter iter(tree);
+        iter._branch_iter.reset(nullptr);
+        iter._state = state::DONE;
+
+        return iter;
+    }
+
+    template<typename T>
+    bool binary_tree<T>::binary_tree_iter::operator==(const binary_tree<T>::binary_tree_iter &other) const
+    {
+        if (&_tree != &other._tree)
+            return false;
+        if (_state != other._state)
+            return false;
+        return _branch_iter == other._branch_iter;
+    }
+
+
+    template<typename T>
+    bool binary_tree<T>::binary_tree_iter::operator!=(const binary_tree_iter &other) const
+    {
+        return !(*this == other);
+    }
+
+    template<typename T>
+    const T& binary_tree<T>::binary_tree_iter::operator*() const
+    {
+        switch (_state)
+        {
+        case state::DONE:
+            throw std::out_of_range("Access of iterator after end");
+        case state::MIDDLE:
+            return _tree.data();
+        case state::LEFT:
+        case state::RIGHT:
+            return _branch_iter->operator*();
+        default:
+            throw std::logic_error("Missing switch value");
+        }
+    }
+
+    template<typename T>
+    const T* binary_tree<T>::binary_tree_iter::operator->() const
+    {
+        return &this->operator*();
+    }
+
+    template<typename T>
+    typename binary_tree<T>::binary_tree_iter& binary_tree<T>::binary_tree_iter::operator++()
+    {
+        switch (_state)
+        {
+        case state::DONE:
+            throw std::out_of_range("Cannot advance iterator after end");
+        case state::MIDDLE:
+            if (!_tree.right())
+            {
+                _state = state::DONE;
+                break;
+            }
+            _state = state::RIGHT;
+            _branch_iter = binary_tree_iter_ptr(new binary_tree_iter(*_tree.right()));
+            break;
+        case state::LEFT:
+            advance_branch_iter(state::MIDDLE);
+            break;
+        case state::RIGHT:
+            advance_branch_iter(state::DONE);
+            break;
+        default:
+            throw std::logic_error("Missing switch value");
+        }
+
+        return *this;
+    }
+
+    template<typename T>
+    void binary_tree<T>::binary_tree_iter::advance_branch_iter(state next_state)
+    {
+        _branch_iter->operator++();
+        if (_branch_iter->_state == state::DONE)
+        {
+            _branch_iter.reset(nullptr);
+            _state = next_state;
+        }
+    }
+
+}
+
+#endif // _BINARY_SEARCH_TREE_H_
