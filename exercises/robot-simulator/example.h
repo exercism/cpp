3c734f572c5223e049eb13afce6ec55182a94445
chgraef <1595191+chgraef@users.noreply.github.com>
2019-03-23 17:06:59 +0100

    Implement exercise robot-simulator (#166)
    * Implement exercise robot-simulator
    
    * Configure exercise robot-simulator
    
    * Fix broken Travis build
    
    * Use namespace block definition instead of full qualification
    
    * Merge CMakeLists.txt
    
    * Update tests based on latest specification
    
    * Change function parameter to pass-by-reference
    
    * Introduce using declarations to save some text
    
    * Update config.json
    
    * Update readme
    
    * Tidy up for coherent look
    
    * Reduce verbosity of tests

---
+++ exercises/robot-simulator/example.h
@@ -0,0 +1,34 @@
+#if !defined(ROBOT_SIMULATOR_H)
+#define ROBOT_SIMULATOR_H
+#include <string>
+#include <utility>
+
+namespace robot_simulator {
+
+enum class Bearing {NORTH, WEST, SOUTH, EAST};
+
+class Robot 
+{
+public:
+    Robot();
+    Robot(std::pair<int, int>, Bearing);
+    Robot(Robot const&) = delete;
+    Robot& operator=(Robot const&) = delete;
+    Robot(Robot const&&) = delete;
+    Robot& operator=(Robot const&&) = delete;
+    ~Robot() = default;
+    void turn_left(); 
+    void turn_right();
+    void advance();
+    void execute_sequence(std::string const&);
+    std::pair<int, int> get_position() const;
+    Bearing get_bearing() const;
+private:
+    std::pair<int, int> robot_position;
+    Bearing robot_bearing;
+};
+
+} // namespace robot_simulator
+
+#endif // ROBOT_SIMULATOR
+
