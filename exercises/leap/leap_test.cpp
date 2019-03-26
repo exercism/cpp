c0f071c4db18d27ec96c64958b12203b5a427fd1
Henry Lee <lee0906@hotmail.com>
2019-03-27 09:58:10 +1100

    Update the test cases in the leap exercise
    Sync the test cases from the canonical test data

--- exercises/leap/leap_test.cpp
+++ exercises/leap/leap_test.cpp
@@ -2,34 +2,34 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(a_known_leap_year)
+BOOST_AUTO_TEST_CASE(not_divisible_by_4)
+{
+    BOOST_REQUIRE(!leap::is_leap_year(2015));
+}
+
+#if defined(EXERCISM_RUN_ALL_TESTS)
+BOOST_AUTO_TEST_CASE(divisible_by_2_not_divisible_by_4)
+{
+    BOOST_REQUIRE(!leap::is_leap_year(1970));
+}
+
+BOOST_AUTO_TEST_CASE(divisible_by_4_not_divisible_by_100)
 {
     BOOST_REQUIRE(leap::is_leap_year(1996));
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
-BOOST_AUTO_TEST_CASE(any_old_year)
+BOOST_AUTO_TEST_CASE(divisible_by_100_not_divisible_by_400)
 {
-    BOOST_REQUIRE(!leap::is_leap_year(1997));
+    BOOST_REQUIRE(!leap::is_leap_year(2100));
 }
 
-BOOST_AUTO_TEST_CASE(turn_of_the_20th_century)
-{
-    BOOST_REQUIRE(!leap::is_leap_year(1900));
-}
-
-BOOST_AUTO_TEST_CASE(turn_of_the_21st_century)
+BOOST_AUTO_TEST_CASE(divisible_by_400)
 {
     BOOST_REQUIRE(leap::is_leap_year(2000));
 }
 
-BOOST_AUTO_TEST_CASE(turn_of_the_23th_century)
+BOOST_AUTO_TEST_CASE(divisible_by_200_not_divisible_by_400)
 {
-    BOOST_REQUIRE(!leap::is_leap_year(2200));
-}
-
-BOOST_AUTO_TEST_CASE(turn_of_the_25th_century)
-{
-    BOOST_REQUIRE(leap::is_leap_year(2400));
+    BOOST_REQUIRE(!leap::is_leap_year(1800));
 }
 #endif
