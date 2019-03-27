7553c0725290ba08e46c0235c6e65b71e69829d6
Patrick Jackson <PatrickSJackson@gmail.com>
2019-03-26 20:19:03 -0700

    leap: Onboard to Catch2.

--- exercises/leap/leap_test.cpp
+++ exercises/leap/leap_test.cpp
@@ -1,35 +1,35 @@
 #include "leap.h"
-#define BOOST_TEST_MAIN
-#include <boost/test/unit_test.hpp>
+#define CATCH_CONFIG_MAIN
+#include "test/catch.hpp"
 
-BOOST_AUTO_TEST_CASE(not_divisible_by_4)
+TEST_CASE("not_divisible_by_4")
 {
-    BOOST_REQUIRE(!leap::is_leap_year(2015));
+    REQUIRE(!leap::is_leap_year(2015));
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
-BOOST_AUTO_TEST_CASE(divisible_by_2_not_divisible_by_4)
+TEST_CASE("divisible_by_2_not_divisible_by_4")
 {
-    BOOST_REQUIRE(!leap::is_leap_year(1970));
+    REQUIRE(!leap::is_leap_year(1970));
 }
 
-BOOST_AUTO_TEST_CASE(divisible_by_4_not_divisible_by_100)
+TEST_CASE("divisible_by_4_not_divisible_by_100")
 {
-    BOOST_REQUIRE(leap::is_leap_year(1996));
+    REQUIRE(leap::is_leap_year(1996));
 }
 
-BOOST_AUTO_TEST_CASE(divisible_by_100_not_divisible_by_400)
+TEST_CASE("divisible_by_100_not_divisible_by_400")
 {
-    BOOST_REQUIRE(!leap::is_leap_year(2100));
+    REQUIRE(!leap::is_leap_year(2100));
 }
 
-BOOST_AUTO_TEST_CASE(divisible_by_400)
+TEST_CASE("divisible_by_400")
 {
-    BOOST_REQUIRE(leap::is_leap_year(2000));
+    REQUIRE(leap::is_leap_year(2000));
 }
 
-BOOST_AUTO_TEST_CASE(divisible_by_200_not_divisible_by_400)
+TEST_CASE("divisible_by_200_not_divisible_by_400")
 {
-    BOOST_REQUIRE(!leap::is_leap_year(1800));
+    REQUIRE(!leap::is_leap_year(1800));
 }
 #endif
