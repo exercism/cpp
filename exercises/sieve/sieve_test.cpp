bce9082396b64bd020d2005fede7ffb77a2dfbb6
Patrick Jackson <PatrickSJackson@gmail.com>
2019-04-06 15:16:01 -0700

    sieve: Onboard to Catch2.

--- exercises/sieve/sieve_test.cpp
+++ exercises/sieve/sieve_test.cpp
@@ -1,45 +1,45 @@
 #include "sieve.h"
-#define BOOST_TEST_MAIN
-#include <boost/test/unit_test.hpp>
+#define CATCH_CONFIG_MAIN
+#include "test/catch.hpp"
 
-BOOST_AUTO_TEST_CASE(no_primes_under_two)
+TEST_CASE("no_primes_under_two")
 {
     const std::vector<int> expected{};
 
     const std::vector<int> actual = sieve::primes(1);
 
-    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), actual.begin(), actual.end());
+    REQUIRE(expected == actual);
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
-BOOST_AUTO_TEST_CASE(find_first_prime)
+TEST_CASE("find_first_prime")
 {
     const std::vector<int> expected{2};
 
     const std::vector<int> actual = sieve::primes(2);
 
-    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), actual.begin(), actual.end());
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(find_primes_up_to_10)
+TEST_CASE("find_primes_up_to_10")
 {
     const std::vector<int> expected{2, 3, 5, 7};
 
     const std::vector<int> actual = sieve::primes(10);
 
-    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), actual.begin(), actual.end());
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(limit_is_prime)
+TEST_CASE("limit_is_prime")
 {
     const std::vector<int> expected{2, 3, 5, 7, 11, 13};
 
     const std::vector<int> actual = sieve::primes(13);
 
-    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), actual.begin(), actual.end());
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(find_primes_up_to_1000)
+TEST_CASE("find_primes_up_to_1000")
 {
     const std::vector<int> expected{
         2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
@@ -59,6 +59,6 @@ BOOST_AUTO_TEST_CASE(find_primes_up_to_1000)
 
     const std::vector<int> actual = sieve::primes(1000);
 
-    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), actual.begin(), actual.end());
+    REQUIRE(expected == actual);
 }
 #endif
