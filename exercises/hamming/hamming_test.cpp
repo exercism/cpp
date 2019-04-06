cad5d8d2ac686f3e85421e887a10bf5215c3dfeb
Patrick Jackson <PatrickSJackson@gmail.com>
2019-04-06 14:22:01 -0700

    hamming: Onboard to Catch2.

--- exercises/hamming/hamming_test.cpp
+++ exercises/hamming/hamming_test.cpp
@@ -1,51 +1,51 @@
 #include "hamming.h"
 #include <stdexcept>
-#define BOOST_TEST_MAIN
-#include <boost/test/unit_test.hpp>
+#define CATCH_CONFIG_MAIN
+#include "test/catch.hpp"
 
-BOOST_AUTO_TEST_CASE(no_difference_between_identical_strands)
+TEST_CASE("no_difference_between_identical_strands")
 {
-    BOOST_REQUIRE_EQUAL(0, hamming::compute("A", "A"));
+    REQUIRE(0 == hamming::compute("A", "A"));
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
-BOOST_AUTO_TEST_CASE(complete_hamming_distance_for_single_nucleotide_strand)
+TEST_CASE("complete_hamming_distance_for_single_nucleotide_strand")
 {
-    BOOST_REQUIRE_EQUAL(1, hamming::compute("A", "G"));
+    REQUIRE(1 == hamming::compute("A", "G"));
 }
 
-BOOST_AUTO_TEST_CASE(complete_hamming_distance_for_small_strand)
+TEST_CASE("complete_hamming_distance_for_small_strand")
 {
-    BOOST_REQUIRE_EQUAL(2, hamming::compute("AG", "CT"));
+    REQUIRE(2 == hamming::compute("AG", "CT"));
 }
 
-BOOST_AUTO_TEST_CASE(small_hamming_distance)
+TEST_CASE("small_hamming_distance")
 {
-    BOOST_REQUIRE_EQUAL(1, hamming::compute("AT", "CT"));
+    REQUIRE(1 == hamming::compute("AT", "CT"));
 }
 
-BOOST_AUTO_TEST_CASE(small_hamming_distance_in_longer_strand)
+TEST_CASE("small_hamming_distance_in_longer_strand")
 {
-    BOOST_REQUIRE_EQUAL(1, hamming::compute("GGACG", "GGTCG"));
+    REQUIRE(1 == hamming::compute("GGACG", "GGTCG"));
 }
 
-BOOST_AUTO_TEST_CASE(domain_error_when_first_string_is_longer)
+TEST_CASE("domain_error_when_first_string_is_longer")
 {
-    BOOST_REQUIRE_THROW(hamming::compute("AAAG", "AAA"), std::domain_error);
+    REQUIRE_THROWS_AS(hamming::compute("AAAG", "AAA"), std::domain_error);
 }
 
-BOOST_AUTO_TEST_CASE(domain_error_when_second_string_is_longer)
+TEST_CASE("domain_error_when_second_string_is_longer")
 {
-    BOOST_REQUIRE_THROW(hamming::compute("AAA", "AAAG"), std::domain_error);
+    REQUIRE_THROWS_AS(hamming::compute("AAA", "AAAG"), std::domain_error);
 }
 
-BOOST_AUTO_TEST_CASE(large_hamming_distance)
+TEST_CASE("large_hamming_distance")
 {
-    BOOST_REQUIRE_EQUAL(4, hamming::compute("GATACA", "GCATAA"));
+    REQUIRE(4 == hamming::compute("GATACA", "GCATAA"));
 }
 
-BOOST_AUTO_TEST_CASE(hamming_distance_in_very_long_strand)
+TEST_CASE("hamming_distance_in_very_long_strand")
 {
-    BOOST_REQUIRE_EQUAL(9, hamming::compute("GGACGGATTCTG", "AGGACGGATTCT"));
+    REQUIRE(9 == hamming::compute("GGACGGATTCTG", "AGGACGGATTCT"));
 }
 #endif
