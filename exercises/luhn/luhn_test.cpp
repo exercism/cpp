ef9f95396fde8cdf405c4995500e8754a9f68d99
Patrick Jackson <PatrickSJackson@gmail.com>
2019-04-08 07:55:04 -0700

    luhn: Onboard to Catch2.

--- exercises/luhn/luhn_test.cpp
+++ exercises/luhn/luhn_test.cpp
@@ -1,150 +1,150 @@
 #include "luhn.h"
-#define BOOST_TEST_MAIN
-#include <boost/test/unit_test.hpp>
+#define CATCH_CONFIG_MAIN
+#include "test/catch.hpp"
 
 //Luhn exercise test case data version 1.5.0
 
-BOOST_AUTO_TEST_CASE(single_digit_strings_can_not_be_valid)
+TEST_CASE("single_digit_strings_can_not_be_valid")
 {
     const bool actual = luhn::valid("1");
 
     const bool expected {false};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
-BOOST_AUTO_TEST_CASE(a_single_zero_is_invalid)
+TEST_CASE("a_single_zero_is_invalid")
 {
     const bool actual = luhn::valid("0");
 
     const bool expected {false};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(a_simple_valid_SIN_that_remains_valid_if_reversed)
+TEST_CASE("a_simple_valid_SIN_that_remains_valid_if_reversed")
 {
     const bool actual = luhn::valid("059");
 
     const bool expected {true};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(a_simple_valid_SIN_that_becomes_invalid_if_reversed)
+TEST_CASE("a_simple_valid_SIN_that_becomes_invalid_if_reversed")
 {
     const bool actual = luhn::valid("59");
 
     const bool expected {true};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(a_valid_Canadian_SIN)
+TEST_CASE("a_valid_Canadian_SIN")
 {
     const bool actual = luhn::valid("055 444 285");
 
     const bool expected {true};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(invalid_Canadian_SIN)
+TEST_CASE("invalid_Canadian_SIN")
 {
     const bool actual = luhn::valid("055 444 286");
 
     const bool expected {false};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(invalid_credit_card)
+TEST_CASE("invalid_credit_card")
 {
     const bool actual = luhn::valid("8273 1232 7352 0569");
 
     const bool expected {false};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(valid_number_with_an_even_number_of_digits)
+TEST_CASE("valid_number_with_an_even_number_of_digits")
 {
     const bool actual = luhn::valid("095 245 88");
 
     const bool expected {true};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(valid_strings_with_a_non_digit_added_at_the_end_become_invalid)
+TEST_CASE("valid_strings_with_a_non_digit_added_at_the_end_become_invalid")
 {
     const bool actual = luhn::valid("059a");
 
     const bool expected {false};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
-BOOST_AUTO_TEST_CASE(valid_strings_with_punctuation_included_become_invalid)
+TEST_CASE("valid_strings_with_punctuation_included_become_invalid")
 {
     const bool actual = luhn::valid("055-444-285");
 
     const bool expected {false};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(valid_strings_with_symbols_included_become_invalid)
+TEST_CASE("valid_strings_with_symbols_included_become_invalid")
 {
     const bool actual = luhn::valid("055£ 444$ 285");
 
     const bool expected {false};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(single_zero_with_spaces_invalid)
+TEST_CASE("single_zero_with_spaces_invalid")
 {
     const bool actual = luhn::valid(" 0");
 
     const bool expected {false};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(more_than_a_single_zero_is_valid)
+TEST_CASE("more_than_a_single_zero_is_valid")
 {
     const bool actual = luhn::valid("0000 0");
 
     const bool expected {true};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(input_digit_9_is_correctly_converted_to_output_digit_9)
+TEST_CASE("input_digit_9_is_correctly_converted_to_output_digit_9")
 {
     const bool actual = luhn::valid("091");
 
     const bool expected {true};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(using_ascii_value_for_non_doubled_non_digit_is_not_allowed)
+TEST_CASE("using_ascii_value_for_non_doubled_non_digit_is_not_allowed")
 {
     const bool actual = luhn::valid("055b 444 285");
 
     const bool expected {false};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(strings_with_non_digits_is_invalid)
+TEST_CASE("strings_with_non_digits_is_invalid")
 {
     const bool actual = luhn::valid(":9");
 
     const bool expected {false};
 
-    BOOST_TEST(expected == actual);
+    REQUIRE(expected == actual);
 }
 #endif
