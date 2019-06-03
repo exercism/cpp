ce01e72c580088f64c468b4ed914a0c8086a7086
Alexander Smurov <marvelou-s@ya.ru>
2019-06-03 12:08:38 +0300

    Corrected include guard

--- exercises/armstrong-numbers/example.h
+++ exercises/armstrong-numbers/example.h
@@ -1,4 +1,4 @@
-#ifndef ARMSTRONG_NUMBERS_H_
+#if !defined(ARMSTRONG_NUMBERS_H_)
 #define ARMSTRONG_NUMBERS_H_
 
 namespace armstrong_numbers {
@@ -7,4 +7,4 @@ bool is_armstrong_number(int number);
 
 } // namespace armstrong_numbers
 
-#endif //!ARMSTRONG_NUMBERS_
+#endif // !ARMSTRONG_NUMBERS_H_
