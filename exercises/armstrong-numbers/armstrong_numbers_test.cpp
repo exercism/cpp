a12a18db6e072197227e146a91de45487c369bb0
Alexander Smurov <marvelou-s@ya.ru>
2019-06-03 12:06:07 +0300

    Corrected vertical spacing. Used REQUIRE_FALSE for clarity

--- exercises/armstrong-numbers/armstrong_numbers_test.cpp
+++ exercises/armstrong-numbers/armstrong_numbers_test.cpp
@@ -1,14 +1,14 @@
 #include "test/catch.hpp"
 #include "armstrong_numbers.h"
 
-// Armstrong-number exercise test case data version 1.1.0
+// Armstrong-numbers exercise test case data version 1.1.0
+
 TEST_CASE("zero_is_an_armstrong_number")
 {
     REQUIRE(armstrong_numbers::is_armstrong_number(0));
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
-
 TEST_CASE("single_digit_numbers_are_armstrong_numbers")
 {
     REQUIRE(armstrong_numbers::is_armstrong_number(5));
@@ -16,7 +16,7 @@ TEST_CASE("single_digit_numbers_are_armstrong_numbers")
 
 TEST_CASE("there_are_no_2_digit_armstrong_numbers")
 {
-    REQUIRE(!armstrong_numbers::is_armstrong_number(10));
+    REQUIRE_FALSE(armstrong_numbers::is_armstrong_number(10));
 }
 
 TEST_CASE("three_digit_number_that_is_an_armstrong_number")
@@ -26,7 +26,7 @@ TEST_CASE("three_digit_number_that_is_an_armstrong_number")
 
 TEST_CASE("three_digit_number_that_is_not_an_armstrong_number")
 {
-    REQUIRE(!armstrong_numbers::is_armstrong_number(100));
+    REQUIRE_FALSE(armstrong_numbers::is_armstrong_number(100));
 }
 
 TEST_CASE("four_digit_number_that_is_an_armstrong_number")
@@ -36,7 +36,7 @@ TEST_CASE("four_digit_number_that_is_an_armstrong_number")
 
 TEST_CASE("four_digit_number_that_is_not_an_armstrong_number")
 {
-    REQUIRE(!armstrong_numbers::is_armstrong_number(9475));
+    REQUIRE_FALSE(armstrong_numbers::is_armstrong_number(9475));
 }
 
 TEST_CASE("seven_digit_number_that_is_an_armstrong_number")
@@ -46,7 +46,6 @@ TEST_CASE("seven_digit_number_that_is_an_armstrong_number")
 
 TEST_CASE("seven_digit_number_that_is_not_an_armstrong_number")
 {
-    REQUIRE(!armstrong_numbers::is_armstrong_number(9926314));
+    REQUIRE_FALSE(armstrong_numbers::is_armstrong_number(9926314));
 }
-
-#endif //!EXERCISM_RUN_ALL_TESTS
+#endif // !EXERCISM_RUN_ALL_TESTS
