394cb2e0798bbbc8545e262007d75768219a209e
Alexander Smurov <marvelou-s@ya.ru>
2019-06-03 12:14:36 +0300

    Made example solution more explicitly

--- exercises/armstrong-numbers/example.cpp
+++ exercises/armstrong-numbers/example.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 bool armstrong_numbers::is_armstrong_number(int number) {
-    int length = static_cast<bool>(number) * static_cast<int>(std::log10(std::abs(number))) + 1;
+    int length = number != 0 && number != 1 ? std::log10(std::abs(number)) + 1 : 1;
     int accumulate = 0;
     for (int current = number; current != 0; current /= 10) {
         accumulate += std::pow(current % 10, length);
