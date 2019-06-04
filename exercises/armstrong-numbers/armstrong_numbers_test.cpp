da274625f7a9b7f62edf62b213fa9f33c891caac
Alexander Smurov <marvelou-s@ya.ru>
2019-06-04 04:26:34 +0300

    Implement armstrong numbers (#285)
    * Implement armstrong numbers
    
    * Increased difficulty a bit
    
    * Made changes to the default CMakeLists.txt (based on PR #276)
    
    * Corrected vertical spacing. Used REQUIRE_FALSE for clarity
    
    * Corrected include guard
    
    * Made example solution more explicitly

---
+++ exercises/armstrong-numbers/armstrong_numbers_test.cpp
@@ -0,0 +1,51 @@
+#include "test/catch.hpp"
+#include "armstrong_numbers.h"
+
+// Armstrong-numbers exercise test case data version 1.1.0
+
+TEST_CASE("zero_is_an_armstrong_number")
+{
+    REQUIRE(armstrong_numbers::is_armstrong_number(0));
+}
+
+#if defined(EXERCISM_RUN_ALL_TESTS)
+TEST_CASE("single_digit_numbers_are_armstrong_numbers")
+{
+    REQUIRE(armstrong_numbers::is_armstrong_number(5));
+}
+
+TEST_CASE("there_are_no_2_digit_armstrong_numbers")
+{
+    REQUIRE_FALSE(armstrong_numbers::is_armstrong_number(10));
+}
+
+TEST_CASE("three_digit_number_that_is_an_armstrong_number")
+{
+    REQUIRE(armstrong_numbers::is_armstrong_number(153));
+}
+
+TEST_CASE("three_digit_number_that_is_not_an_armstrong_number")
+{
+    REQUIRE_FALSE(armstrong_numbers::is_armstrong_number(100));
+}
+
+TEST_CASE("four_digit_number_that_is_an_armstrong_number")
+{
+    REQUIRE(armstrong_numbers::is_armstrong_number(9474));
+}
+
+TEST_CASE("four_digit_number_that_is_not_an_armstrong_number")
+{
+    REQUIRE_FALSE(armstrong_numbers::is_armstrong_number(9475));
+}
+
+TEST_CASE("seven_digit_number_that_is_an_armstrong_number")
+{
+    REQUIRE(armstrong_numbers::is_armstrong_number(9926315));
+}
+
+TEST_CASE("seven_digit_number_that_is_not_an_armstrong_number")
+{
+    REQUIRE_FALSE(armstrong_numbers::is_armstrong_number(9926314));
+}
+#endif // !EXERCISM_RUN_ALL_TESTS
