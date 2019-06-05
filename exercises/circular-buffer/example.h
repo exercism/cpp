757c9fadd7d034528860864e26dd4c87a1bb56b6
Alexander Smurov <marvelou-s@ya.ru>
2019-06-05 12:28:01 +0300

    Made template circular_buffer class

--- exercises/circular-buffer/example.h
+++ exercises/circular-buffer/example.h
@@ -2,30 +2,52 @@
 #define CIRCULAR_BUFFER_H_
 
 #include <vector>
-#include <algorithm>
 #include <stdexcept>
 
 namespace circular_buffer {
 
+template <typename ValueType>
 class circular_buffer {
 public:
     circular_buffer(std::size_t capacity) 
-        : buffer_(capacity), occupancy_(capacity), read_position_(0), write_position_(0) {}
-    int read();
-    void write(int item);
-    void overwrite(int item);
-    void clear();
+        : buffer_(capacity), head_(0), tail_(0) {}
+    ValueType read();
+    void write(ValueType item);
+    void overwrite(ValueType item);
+    void clear() { head_ = 0; tail_ = 0; }
 
 private:
-    std::vector<int> buffer_;
-    std::vector<bool> occupancy_;
-    std::size_t read_position_;
-    std::size_t write_position_;
+    std::vector<ValueType> buffer_;
+    std::size_t head_;
+    std::size_t tail_;
 
-    void insert(int item);
-    bool is_empty() const;
-    bool is_full() const;
+    void insert(ValueType item);
+    bool is_empty() const { return head_ == tail_; }
+    bool is_full() const { return head_ == (tail_ + 1) % (buffer_.size() + 1); }
 };
+
+template <typename ValueType>
+ValueType circular_buffer<ValueType>::read() {
+    if (is_empty()) throw std::domain_error("Circular buffer is empty."); 
+    return buffer_[head_++ % buffer_.size()];
+}
+
+template <typename ValueType>
+void circular_buffer<ValueType>::write(ValueType item) {
+    if (is_full()) throw std::domain_error("Circular buffer is full.");
+    insert(item);
+}
+
+template <typename ValueType>
+void circular_buffer<ValueType>::overwrite(ValueType item) {
+    if (is_full()) head_ = ++head_ % buffer_.size();
+    insert(item);
+}
+
+template <typename ValueType>
+void circular_buffer<ValueType>::insert(ValueType item) {
+    buffer_[tail_++ % buffer_.size()] = item;
+}
 
 } // namespace circular_buffer
 
