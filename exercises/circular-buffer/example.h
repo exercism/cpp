69779fc50825fb2be67438d3664ef130048456e5
Alexander Smurov <marvelou-s@ya.ru>
2019-06-13 12:34:36 +0300

    Fixed bug when full buffer can be written after overwrite

--- exercises/circular-buffer/example.h
+++ exercises/circular-buffer/example.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <stdexcept>
+#include <iostream>
 
 namespace circular_buffer {
 
@@ -10,43 +11,47 @@ template <typename ValueType>
 class circular_buffer {
 public:
     circular_buffer(std::size_t capacity) 
-        : buffer_(capacity), head_(0), tail_(0) {}
+        : buffer_(capacity + 1), head_(0), tail_(0) {}
     ValueType read();
     void write(ValueType item);
     void overwrite(ValueType item);
-    void clear() { head_ = 0; tail_ = 0; }
+    void clear() { head_ = tail_; }
 
 private:
     std::vector<ValueType> buffer_;
     std::size_t head_;
     std::size_t tail_;
 
-    void insert(ValueType item);
+    void push_back(ValueType item);
+    void move_position(std::size_t& position) { position = (position + 1) % buffer_.size(); }
     bool is_empty() const { return head_ == tail_; }
-    bool is_full() const { return head_ == (tail_ + 1) % (buffer_.size() + 1); }
+    bool is_full() const { return head_ == (tail_ + 1) % buffer_.size(); }
 };
 
 template <typename ValueType>
 ValueType circular_buffer<ValueType>::read() {
     if (is_empty()) throw std::domain_error("Circular buffer is empty."); 
-    return buffer_[head_++ % buffer_.size()];
+    ValueType item = buffer_[head_];
+    move_position(head_);
+    return item;
 }
 
 template <typename ValueType>
 void circular_buffer<ValueType>::write(ValueType item) {
     if (is_full()) throw std::domain_error("Circular buffer is full.");
-    insert(item);
+    push_back(item);
 }
 
 template <typename ValueType>
 void circular_buffer<ValueType>::overwrite(ValueType item) {
-    if (is_full()) head_ = (head_ + 1) % buffer_.size();
-    insert(item);
+    if (is_full()) move_position(head_);
+    push_back(item);
 }
 
 template <typename ValueType>
-void circular_buffer<ValueType>::insert(ValueType item) {
-    buffer_[tail_++ % buffer_.size()] = item;
+void circular_buffer<ValueType>::push_back(ValueType item) {
+    buffer_[tail_] = item;
+    move_position(tail_);
 }
 
 } // namespace circular_buffer
