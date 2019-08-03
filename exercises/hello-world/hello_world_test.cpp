fb976774f713b6e7230cbbcfe1f390978925f4ac
Patrick Jackson <PatrickSJackson@gmail.com>
2019-08-03 14:39:26 -0700

    Improves Hello-World (#279)
    * Adding comments and a hello world example that compiles.
    
    * Adding CI test for the compilable, but test failing, hello-world.
    
    * Cleaning up hello().
    
    * Improving include guard description.

--- exercises/hello-world/hello_world_test.cpp
+++ exercises/hello-world/hello_world_test.cpp
@@ -1,7 +1,12 @@
+// Include the header file with the definitions of the functions you create.
 #include "hello_world.h"
+
+// Include the test framework.
 #include "test/catch.hpp"
 
+// Declares a single test.
 TEST_CASE("test_hello")
 {
-    REQUIRE("Hello, World!" == hello_world::hello());
+    // Check if your function returns "Hello, World!".
+    REQUIRE(hello_world::hello() == "Hello, World!");
 }
