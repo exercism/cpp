7a768b61bd8296e3207e9195ec9c8cfebf22618e
Patrick Jackson <PatrickSJackson@gmail.com>
2019-08-17 09:58:42 -0700

    rna_transcription: Fix-up namespace

--- exercises/rna-transcription/rna_transcription_test.cpp
+++ exercises/rna-transcription/rna_transcription_test.cpp
@@ -3,27 +3,27 @@
 
 TEST_CASE("transcribes_cytidine_to_guanosine")
 {
-    REQUIRE('G' == transcription::to_rna('C'));
+    REQUIRE('G' == rna_transcription::to_rna('C'));
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
 TEST_CASE("transcribes_guanosine_to_cytidine")
 {
-    REQUIRE('C' == transcription::to_rna('G'));
+    REQUIRE('C' == rna_transcription::to_rna('G'));
 }
 
 TEST_CASE("transcribes_adenosine_to_uracil")
 {
-    REQUIRE('U' == transcription::to_rna('A'));
+    REQUIRE('U' == rna_transcription::to_rna('A'));
 }
 
 TEST_CASE("transcribes_thymidine_to_adenosine")
 {
-    REQUIRE('A' == transcription::to_rna('T'));
+    REQUIRE('A' == rna_transcription::to_rna('T'));
 }
 
 TEST_CASE("transcribes_all_dna_nucleotides_to_their_rna_complements")
 {
-    REQUIRE("UGCACCAGAAUU" == transcription::to_rna("ACGTGGTCTTAA"));
+    REQUIRE("UGCACCAGAAUU" == rna_transcription::to_rna("ACGTGGTCTTAA"));
 }
 #endif
