cb462dd85657a1bf07fc69775e2c5d21a0cfec18
Patrick Jackson <PatrickSJackson@gmail.com>
2019-08-17 09:58:41 -0700

    atbash_cipher: Fix-up namespace

--- exercises/atbash-cipher/atbash_cipher_test.cpp
+++ exercises/atbash-cipher/atbash_cipher_test.cpp
@@ -3,65 +3,65 @@
 
 TEST_CASE("encode_yes")
 {
-    REQUIRE("bvh" == atbash::encode("yes"));
+    REQUIRE("bvh" == atbash_cipher::encode("yes"));
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
 TEST_CASE("encode_no")
 {
-    REQUIRE("ml" == atbash::encode("no"));
+    REQUIRE("ml" == atbash_cipher::encode("no"));
 }
 
 TEST_CASE("encode_OMG")
 {
-    REQUIRE("lnt" == atbash::encode("OMG"));
+    REQUIRE("lnt" == atbash_cipher::encode("OMG"));
 }
 
 TEST_CASE("encode_spaces")
 {
-    REQUIRE("lnt" == atbash::encode("O M G"));
+    REQUIRE("lnt" == atbash_cipher::encode("O M G"));
 }
 
 TEST_CASE("encode_mindblowingly")
 {
-    REQUIRE("nrmwy oldrm tob" == atbash::encode("mindblowingly"));
+    REQUIRE("nrmwy oldrm tob" == atbash_cipher::encode("mindblowingly"));
 }
 
 TEST_CASE("encode_numbers")
 {
-    REQUIRE("gvhgr mt123 gvhgr mt" == atbash::encode("Testing,1 2 3, testing."));
+    REQUIRE("gvhgr mt123 gvhgr mt" == atbash_cipher::encode("Testing,1 2 3, testing."));
 }
 
 TEST_CASE("encode_deep_thought")
 {
-    REQUIRE("gifgs rhurx grlm" == atbash::encode("Truth is fiction."));
+    REQUIRE("gifgs rhurx grlm" == atbash_cipher::encode("Truth is fiction."));
 }
 
 TEST_CASE("encode_all_the_letters")
 {
     REQUIRE("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt" ==
-                        atbash::encode("The quick brown fox jumps over the lazy dog."));
+                        atbash_cipher::encode("The quick brown fox jumps over the lazy dog."));
 }
 
 TEST_CASE("decode_exercism")
 {
-    REQUIRE("exercism" == atbash::decode("vcvix rhn"));
+    REQUIRE("exercism" == atbash_cipher::decode("vcvix rhn"));
 }
 
 TEST_CASE("decode_a_sentence")
 {
     REQUIRE("anobstacleisoftenasteppingstone" ==
-                        atbash::decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v"));
+                        atbash_cipher::decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v"));
 }
 
 TEST_CASE("decode_numbers")
 {
-    REQUIRE("testing123testing" == atbash::decode("gvhgr mt123 gvhgr mt"));
+    REQUIRE("testing123testing" == atbash_cipher::decode("gvhgr mt123 gvhgr mt"));
 }
 
 TEST_CASE("decode_all_the_letters")
 {
     REQUIRE("thequickbrownfoxjumpsoverthelazydog" ==
-                        atbash::decode("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"));
+                        atbash_cipher::decode("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"));
 }
 #endif
