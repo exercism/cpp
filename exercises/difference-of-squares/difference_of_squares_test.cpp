a559da5537040c4e054b6f829dffa77e6f77b6c7
Patrick Jackson <PatrickSJackson@gmail.com>
2019-08-17 09:58:41 -0700

    difference_of_squares: Fix-up namespace

--- exercises/difference-of-squares/difference_of_squares_test.cpp
+++ exercises/difference-of-squares/difference_of_squares_test.cpp
@@ -3,23 +3,23 @@
 
 TEST_CASE("up_to_5")
 {
-    REQUIRE(225 == squares::square_of_sum(5));
-    REQUIRE(55 == squares::sum_of_squares(5));
-    REQUIRE(170 == squares::difference(5));
+    REQUIRE(225 == difference_of_squares::square_of_sum(5));
+    REQUIRE(55 == difference_of_squares::sum_of_squares(5));
+    REQUIRE(170 == difference_of_squares::difference(5));
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
 TEST_CASE("up_to_10")
 {
-    REQUIRE(3025 == squares::square_of_sum(10));
-    REQUIRE(385 == squares::sum_of_squares(10));
-    REQUIRE(2640 == squares::difference(10));
+    REQUIRE(3025 == difference_of_squares::square_of_sum(10));
+    REQUIRE(385 == difference_of_squares::sum_of_squares(10));
+    REQUIRE(2640 == difference_of_squares::difference(10));
 }
 
 TEST_CASE("up_to_100")
 {
-    REQUIRE(25502500 == squares::square_of_sum(100));
-    REQUIRE(338350 == squares::sum_of_squares(100));
-    REQUIRE(25164150 == squares::difference(100));
+    REQUIRE(25502500 == difference_of_squares::square_of_sum(100));
+    REQUIRE(338350 == difference_of_squares::sum_of_squares(100));
+    REQUIRE(25164150 == difference_of_squares::difference(100));
 }
 #endif
