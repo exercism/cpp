c977361313aba6b792b82f8726610ec85e2f7ad9
Matthias Rahlf <matthias@webding.de>
2019-11-21 03:22:02 +0100

    Implement the exercise "two-fer"
    two-fer is one of the simplest exercises. It's part of the core
    exercises in many language tracks (e.g. JavaScript, Java, Python, Ruby)
    and AFAIK it was the first exercise that got automated mentoring (in
    the Ruby track).
    
    It's simple, but IMHO it's interesting because AFAIK it'd be the only
    C++ exercise where the model solution uses an argument with a default
    value.

---
+++ exercises/two-fer/example.h
@@ -0,0 +1,14 @@
+#if !defined(TWO_FER_H)
+#define TWO_FER_H
+
+#include <string>
+
+namespace two_fer
+{
+    inline std::string two_fer(const std::string& name = "you")
+    {
+        return "One for " + name + ", one for me.";
+    }
+} // namespace two_fer
+
+#endif //TWO_FER_H
