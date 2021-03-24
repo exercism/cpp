3ddbc0bb4688a54fda317d3ad0692b921bfcdbfe
gbudau <54190400+gbudau@users.noreply.github.com>
2021-03-24 20:22:31 +0100

    Fix space_age overflow in implicit constant conversion
    Fixed error: overflow in implicit constant conversion in Space Age

--- exercises/practice/space-age/space_age_test.cpp
+++ exercises/practice/space-age/space_age_test.cpp
@@ -3,9 +3,9 @@
 
 TEST_CASE("age_in_seconds")
 {
-    const space_age::space_age age(1000000);
+    const space_age::space_age age(1000000L);
 
-    REQUIRE(age.seconds() == 1000000);
+    REQUIRE(age.seconds() == 1000000L);
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
@@ -16,7 +16,7 @@ const double accuracy = 0.005;
 
 TEST_CASE("age_in_earth_years")
 {
-    const space_age::space_age age(1000000000);
+    const space_age::space_age age(1000000000L);
 
     // 'Approx' is a helper from the test suite for comparing floating point
     // numbers.
@@ -25,7 +25,7 @@ TEST_CASE("age_in_earth_years")
 
 TEST_CASE("age_in_mercury_years")
 {
-    const space_age::space_age age(2134835688);
+    const space_age::space_age age(2134835688L);
 
     REQUIRE(age.on_earth() == Approx(67.65).margin(accuracy));
     REQUIRE(age.on_mercury() == Approx(280.88).margin(accuracy));
@@ -33,7 +33,7 @@ TEST_CASE("age_in_mercury_years")
 
 TEST_CASE("age_in_venus_years")
 {
-    const space_age::space_age age(189839836);
+    const space_age::space_age age(189839836L);
 
     REQUIRE(age.on_earth() == Approx(6.02).margin(accuracy));
     REQUIRE(age.on_venus() == Approx(9.78).margin(accuracy));
@@ -41,7 +41,7 @@ TEST_CASE("age_in_venus_years")
 
 TEST_CASE("age_in_mars_years")
 {
-    const space_age::space_age age(2329871239);
+    const space_age::space_age age(2329871239L);
 
     REQUIRE(age.on_earth() == Approx(73.83).margin(accuracy));
     REQUIRE(age.on_mars() == Approx(39.25).margin(accuracy));
@@ -49,7 +49,7 @@ TEST_CASE("age_in_mars_years")
 
 TEST_CASE("age_in_jupiter_years")
 {
-    const space_age::space_age age(901876382);
+    const space_age::space_age age(901876382L);
 
     REQUIRE(age.on_earth() == Approx(28.58).margin(accuracy));
     REQUIRE(age.on_jupiter() == Approx(2.41).margin(accuracy));
@@ -57,7 +57,7 @@ TEST_CASE("age_in_jupiter_years")
 
 TEST_CASE("age_in_saturn_years")
 {
-    const space_age::space_age age(3000000000);
+    const space_age::space_age age(3000000000L);
 
     REQUIRE(age.on_earth() == Approx(95.06).margin(accuracy));
     REQUIRE(age.on_saturn() == Approx(3.23).margin(accuracy));
@@ -65,7 +65,7 @@ TEST_CASE("age_in_saturn_years")
 
 TEST_CASE("age_in_uranus_years")
 {
-    const space_age::space_age age(3210123456);
+    const space_age::space_age age(3210123456L);
 
     REQUIRE(age.on_earth() == Approx(101.72).margin(accuracy));
     REQUIRE(age.on_uranus() == Approx(1.21).margin(accuracy));
@@ -73,7 +73,7 @@ TEST_CASE("age_in_uranus_years")
 
 TEST_CASE("age_in_neptune_year")
 {
-    const space_age::space_age age(8210123456);
+    const space_age::space_age age(8210123456L);
 
     REQUIRE(age.on_earth() == Approx(260.16).margin(accuracy));
     REQUIRE(age.on_neptune() == Approx(1.58).margin(accuracy));
