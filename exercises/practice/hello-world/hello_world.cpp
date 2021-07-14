7e75421ab1c195981401ea6a5666315d90a7b5dc
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-14 14:34:13 +0200

    Simplify the `hello-world` exercise's stub. The goal of this is to make things easier for students to get started and to introduce as little syntax as possible. (#466)

--- exercises/practice/hello-world/hello_world.cpp
+++ exercises/practice/hello-world/hello_world.cpp
@@ -1,16 +1,13 @@
 #include "hello_world.h"
 
-// Use everything from the 'std' namespace.
-// This lets us write 'string' instead of 'std::string'.
 using namespace std;
 
-namespace hello_world {
+namespace hello_world
+{
 
-// Define the function itself. This could have also been written as:
-// std::string hello_world::hello()
-string hello() {
-    // Return the string we need.
-    return "Fix me!";
+string hello()
+{
+    return "Goodbye, Mars!";
 }
 
-}  // namespace hello_world
+}
