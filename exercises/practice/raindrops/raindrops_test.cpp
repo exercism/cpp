081da21f9c58d28b9ee55d538962739e125d78a3
Katrina Owen <katrina.owen@gmail.com>
2022-11-14 15:11:59 +0100

    Normalize test descriptions in raindrops
    This updates the raindrops tests to use the same test
    descriptions as the canonical-data in problem-specifications.

--- exercises/practice/raindrops/raindrops_test.cpp
+++ exercises/practice/raindrops/raindrops_test.cpp
@@ -5,78 +5,64 @@
 #include "test/catch.hpp"
 #endif
 
-TEST_CASE("one_yields_itself")
+TEST_CASE("the_sound_for_1_is_1")
 {
     REQUIRE("1" == raindrops::convert(1));
 }
-
 #if defined(EXERCISM_RUN_ALL_TESTS)
-TEST_CASE("three_yields_pling")
+TEST_CASE("the_sound_for_3_is_pling")
 {
     REQUIRE("Pling" == raindrops::convert(3));
 }
-
-TEST_CASE("five_yields_plang")
+TEST_CASE("the_sound_for_5_is_plang")
 {
     REQUIRE("Plang" == raindrops::convert(5));
 }
-
-TEST_CASE("seven_yields_plong")
+TEST_CASE("the_sound_for_7_is_plong")
 {
     REQUIRE("Plong" == raindrops::convert(7));
 }
-
-TEST_CASE("six_yields_pling")
+TEST_CASE("the_sound_for_6_is_pling_as_it_has_a_factor_3")
 {
     REQUIRE("Pling" == raindrops::convert(6));
 }
-
-TEST_CASE("nine_yields_pling")
+TEST_CASE("the_sound_for_9_is_pling_as_it_has_a_factor_3")
 {
     REQUIRE("Pling" == raindrops::convert(9));
 }
-
-TEST_CASE("ten_yields_plang")
+TEST_CASE("the_sound_for_10_is_plang_as_it_has_a_factor_5")
 {
     REQUIRE("Plang" == raindrops::convert(10));
 }
-
-TEST_CASE("fourteen_yields_plong")
+TEST_CASE("the_sound_for_14_is_plong_as_it_has_a_factor_of_7")
 {
     REQUIRE("Plong" == raindrops::convert(14));
 }
-
-TEST_CASE("fifteen_yields_plingplang")
+TEST_CASE("the_sound_for_15_is_plingplang_as_it_has_factors_3_and_5")
 {
     REQUIRE("PlingPlang" == raindrops::convert(15));
 }
-
-TEST_CASE("twenty_one_yields_plingplong")
+TEST_CASE("the_sound_for_21_is_plingplong_as_it_has_factors_3_and_7")
 {
     REQUIRE("PlingPlong" == raindrops::convert(21));
 }
-
-TEST_CASE("twenty_five_yields_plang")
+TEST_CASE("the_sound_for_25_is_plang_as_it_has_a_factor_5")
 {
     REQUIRE("Plang" == raindrops::convert(25));
 }
-
-TEST_CASE("thirty_five_yields_plangplong")
+TEST_CASE("the_sound_for_35_is_plangplong_as_it_has_factors_5_and_7")
 {
     REQUIRE("PlangPlong" == raindrops::convert(35));
 }
-
-TEST_CASE("forty_nine_yields_plong")
+TEST_CASE("the_sound_for_49_is_plong_as_it_has_a_factor_7")
 {
     REQUIRE("Plong" == raindrops::convert(49));
 }
-
-TEST_CASE("fifty_two_yields_itself")
+TEST_CASE("the_sound_for_52_is_52")
 {
     REQUIRE("52" == raindrops::convert(52));
 }
-
-TEST_CASE("one_hundred_five_yields_plingplangplong")
+TEST_CASE("the_sound_for_105_is_plingplangplong_as_it_has_factors_3_5_and_7")
 {
     REQUIRE("PlingPlangPlong" == raindrops::convert(105));
 }
