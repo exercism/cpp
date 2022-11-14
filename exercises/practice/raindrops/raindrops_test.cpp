e7afb83a7217d85663a03d554582d29018c6dd00
Katrina Owen <katrina.owen@gmail.com>
2022-11-14 15:13:07 +0100

    Sync raindrops with problem-specifications
    This brings in updates to the instructions, normalizes the exercise's
    config.json, and adds several new tests.

--- exercises/practice/raindrops/raindrops_test.cpp
+++ exercises/practice/raindrops/raindrops_test.cpp
@@ -26,6 +26,10 @@ TEST_CASE("the_sound_for_6_is_pling_as_it_has_a_factor_3")
 {
     REQUIRE("Pling" == raindrops::convert(6));
 }
+TEST_CASE("2_to_the_power_3_does_not_make_a_raindrop_sound_as_3_is_the_exponent_not_the_base")
+{
+    REQUIRE("8" == raindrops::convert(8));
+}
 TEST_CASE("the_sound_for_9_is_pling_as_it_has_a_factor_3")
 {
     REQUIRE("Pling" == raindrops::convert(9));
@@ -50,6 +54,10 @@ TEST_CASE("the_sound_for_25_is_plang_as_it_has_a_factor_5")
 {
     REQUIRE("Plang" == raindrops::convert(25));
 }
+TEST_CASE("the_sound_for_27_is_pling_as_it_has_a_factor_3")
+{
+    REQUIRE("Pling" == raindrops::convert(27));
+}
 TEST_CASE("the_sound_for_35_is_plangplong_as_it_has_factors_5_and_7")
 {
     REQUIRE("PlangPlong" == raindrops::convert(35));
@@ -65,5 +73,9 @@ TEST_CASE("the_sound_for_52_is_52")
 TEST_CASE("the_sound_for_105_is_plingplangplong_as_it_has_factors_3_5_and_7")
 {
     REQUIRE("PlingPlangPlong" == raindrops::convert(105));
+}
+TEST_CASE("the_sound_for_3125_is_plang_as_it_has_a_factor_5")
+{
+    REQUIRE("Plang" == raindrops::convert(3125));
 }
 #endif
