4f605512e387a8d78363d097da8e63220e56aeb7
Katrina Owen <katrina.owen@gmail.com>
2022-11-15 17:15:56 +0100

    Sync nucleotide-count with problem-specifications (#550)
    * Rectify tests.toml for nucleotide-count
    
    This removes an unimplemented test from the tests.toml.
    
    * Remove extraneous tests from nucleotide-count
    
    This deletes some tests that are not in the canonical-data in
    problem-specifications.
    
    * Reorder nucleotide-count tests
    
    This reorders the tests to match the order in the canonical-data.
    
    * Run configlet fmt on nucleotide-count
    
    This reformats the config.json to make configlet happy.
    
    * Normalize nucleotide-count tests
    
    This updates the existing tests to use the inputs and test names from
    canonical-data.
    
    * Sync nucleotide-count with problem-specifications
    
    This brings the nucleotide-count exercise up to date with
    the specification in problem-specifications.
    
    The sync brought in changes to the instructions and metadata,
    as well as adding a new test.
    
    * Simplify nucleotide-count example solution
    
    The tests no longer reference the count function.
    This deletes it.

--- exercises/practice/nucleotide-count/nucleotide_count_test.cpp
+++ exercises/practice/nucleotide-count/nucleotide_count_test.cpp
@@ -7,10 +7,10 @@
 #include <map>
 #include <stdexcept>
 
-TEST_CASE("has_no_nucleotides")
+TEST_CASE("empty_strand")
 {
     const nucleotide_count::counter dna("");
-    const std::map<char, int> expected{ {'A', 0}, {'T', 0}, {'C', 0}, {'G', 0} };
+    const std::map<char, int> expected{ {'A', 0}, {'C', 0}, {'G', 0}, {'T', 0} };
 
     const auto actual = dna.nucleotide_counts();
 
@@ -18,65 +18,39 @@ TEST_CASE("has_no_nucleotides")
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
-TEST_CASE("has_no_adenosine")
+
+TEST_CASE("can_count_one_nucleotide_in_single_character_input")
 {
-    const nucleotide_count::counter dna("");
-
-    REQUIRE(0 == dna.count('A'));
-}
-
-TEST_CASE("repetitive_cytidine_gets_counts")
-{
-    const nucleotide_count::counter dna("CCCCC");
-
-    REQUIRE(5 == dna.count('C'));
-}
-
-TEST_CASE("repetitive_sequence_has_only_guanosine")
-{
-    const nucleotide_count::counter dna("GGGGGGGG");
-    const std::map<char, int> expected{ {'A', 0}, {'T', 0}, {'C', 0}, {'G', 8} };
+    const nucleotide_count::counter dna("G");
+    const std::map<char, int> expected{ {'A', 0}, {'C', 0}, {'G', 1}, {'T', 0} };
 
     const auto actual = dna.nucleotide_counts();
 
     REQUIRE(expected == actual);
 }
 
-TEST_CASE("counts_only_thymidine")
+TEST_CASE("strand_with_repeated_nucleotide")
 {
-    const nucleotide_count::counter dna("GGGGTAACCCGG");
+    const nucleotide_count::counter dna("GGGGGGG");
+    const std::map<char, int> expected{ {'A', 0}, {'C', 0}, {'G', 7}, {'T', 0} };
 
-    REQUIRE(1 == dna.count('T'));
-}
-
-TEST_CASE("counts_a_nucleotide_only_once")
-{
-    const nucleotide_count::counter dna("GGTTGG");
-
-    dna.count('T');
-
-    REQUIRE(2 == dna.count('T'));
-}
-
-TEST_CASE("validates_nucleotides")
-{
-    const nucleotide_count::counter dna("GGTTGG");
-
-    REQUIRE_THROWS_AS(dna.count('X'), std::invalid_argument);
-}
-
-TEST_CASE("validates_nucleotides_on_construction")
-{
-    REQUIRE_THROWS_AS(nucleotide_count::counter("GGTTGGX"), std::invalid_argument);
-}
-
-TEST_CASE("counts_all_nucleotides")
-{
-    const nucleotide_count::counter dna("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC");
-    std::map<char, int> expected{ {'A', 20}, {'T', 21}, {'G', 17}, {'C', 12} };
-
-    auto actual = dna.nucleotide_counts();
+    const auto actual = dna.nucleotide_counts();
 
     REQUIRE(expected == actual);
+}
+
+TEST_CASE("strand_with_multiple_nucleotides")
+{
+    const nucleotide_count::counter dna("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC");
+    const std::map<char, int> expected{ {'A', 20}, {'C', 12}, {'G', 17}, {'T', 21} };
+
+    const auto actual = dna.nucleotide_counts();
+
+    REQUIRE(expected == actual);
+}
+
+TEST_CASE("strand_with_invalid_nucleotides")
+{
+    REQUIRE_THROWS_AS(nucleotide_count::counter("AGXXACT"), std::invalid_argument);
 }
 #endif
