34c777284ce19edfa8b73f15b007cca7719bd9bd
Christian Willner <34183939+vaeng@users.noreply.github.com>
2023-10-17 16:33:18 +0200

    feat: add pop-count practice exercise (#725)

---
+++ exercises/practice/pop-count/pop_count_test.cpp
@@ -0,0 +1,30 @@
+#include "pop_count.h"
+#ifdef EXERCISM_TEST_SUITE
+#include <catch2/catch.hpp>
+#else
+#include "test/catch.hpp"
+#endif
+
+TEST_CASE("0 eggs")
+{
+    REQUIRE(chicken_coop::positions_to_quantity(0) == 0);
+}
+
+#if defined(EXERCISM_RUN_ALL_TESTS)
+
+TEST_CASE("1 egg")
+{
+    REQUIRE(chicken_coop::positions_to_quantity(16) == 1);
+}
+
+TEST_CASE("4 eggs")
+{
+    REQUIRE(chicken_coop::positions_to_quantity(89) == 4);
+}
+
+TEST_CASE("13 eggs")
+{
+    REQUIRE(chicken_coop::positions_to_quantity(2000000000) == 13);
+}
+
+#endif
