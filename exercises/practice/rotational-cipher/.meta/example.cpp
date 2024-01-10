fc52d2bffd70664a6547d9ce9d58f55bdbbe2c0f
Christian Willner <34183939+vaeng@users.noreply.github.com>
2024-01-10 10:15:20 +0100

    feat: add rotational cipher exercise (#762)

---
+++ exercises/practice/rotational-cipher/.meta/example.cpp
@@ -0,0 +1,22 @@
+#include "rotational_cipher.h"
+
+#include <sstream>
+
+namespace rotational_cipher {
+    std::string rotate(const std::string& text, int shift_key) {
+
+        std::ostringstream encoded_text;
+
+        for(char c : text) {
+            if (c >= 'a' && c <= 'z') {
+                c = (c + shift_key - 'a') % 26 + 'a';
+            }
+            else if (c >= 'A' && c <= 'Z') {
+                c = (c + shift_key - 'A') % 26 + 'A';
+            }
+            encoded_text << c;
+        }
+        return encoded_text.str();
+    }
+
+}  // namespace rotational_cipher
