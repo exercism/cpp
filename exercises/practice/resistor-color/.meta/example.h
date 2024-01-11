be7fc9e6044206cbaa9ca4d2a1152932a71fc8f5
vaeng <34183939+vaeng@users.noreply.github.com>
2024-01-11 22:48:41 +0100

    feat: add resistor-color exercise

---
+++ exercises/practice/resistor-color/.meta/example.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace resistor_color {
+    int color_code(const std::string& color);
+    std::vector<std::string> colors();
+}  // namespace resistor_color
