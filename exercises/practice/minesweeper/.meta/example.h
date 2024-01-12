663a906e921e91652d5e978a610e8549e4b0a7ab
vaeng <34183939+vaeng@users.noreply.github.com>
2024-01-12 12:44:49 +0100

    feat: add minesweeper exercise

---
+++ exercises/practice/minesweeper/.meta/example.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace minesweeper {
+    std::vector<std::string> annotate(const std::vector<std::string>& minefield);
+}  // namespace minesweeper
