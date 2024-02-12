d43cbd4beb1cdea30f449632e12aba1196cd61d7
vaeng <34183939+vaeng@users.noreply.github.com>
2024-02-12 22:33:40 +0100

    feat: add skeleton to log exercise stub

--- exercises/concept/log-levels/log_levels.cpp
+++ exercises/concept/log-levels/log_levels.cpp
@@ -1,5 +1,15 @@
 #include <string>
 
 namespace log_line {
+    std::string message(std::string line) {
+        // return the message
+    }
 
-} // namespace log_line
+    std::string log_level(std::string line) {
+        // return the log level
+    }
+
+    std::string reformat(std::string line) {
+        // return the reformatted message
+    }
+}
