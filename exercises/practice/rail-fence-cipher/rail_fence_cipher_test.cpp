bbc4237b887aefb2ddb804f1658eb743e6a62cd5
Alexey Klimkin <klimkin@gmail.com>
2024-06-30 16:15:24 -0700

    Update with configlet and clang-format

--- exercises/practice/rail-fence-cipher/rail_fence_cipher_test.cpp
+++ exercises/practice/rail-fence-cipher/rail_fence_cipher_test.cpp
@@ -6,17 +6,20 @@
 #endif
 
 TEST_CASE("encode with one rail, same output as input", "") {
-    REQUIRE(rail_fence_cipher::encode("XOXOXOXOXOXOXOXOXO", 1) == "XOXOXOXOXOXOXOXOXO");
+    REQUIRE(rail_fence_cipher::encode("XOXOXOXOXOXOXOXOXO", 1) ==
+            "XOXOXOXOXOXOXOXOXO");
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
 
 TEST_CASE("encode with two rails", "") {
-    REQUIRE(rail_fence_cipher::encode("XOXOXOXOXOXOXOXOXO", 2) == "XXXXXXXXXOOOOOOOOO");
+    REQUIRE(rail_fence_cipher::encode("XOXOXOXOXOXOXOXOXO", 2) ==
+            "XXXXXXXXXOOOOOOOOO");
 }
 
 TEST_CASE("encode with three rails", "") {
-    REQUIRE(rail_fence_cipher::encode("WEAREDISCOVEREDFLEEATONCE", 3) == "WECRLTEERDSOEEFEAOCAIVDEN");
+    REQUIRE(rail_fence_cipher::encode("WEAREDISCOVEREDFLEEATONCE", 3) ==
+            "WECRLTEERDSOEEFEAOCAIVDEN");
 }
 
 TEST_CASE("encode with ending in the middle", "") {
@@ -24,16 +27,20 @@ TEST_CASE("encode with ending in the middle", "") {
 }
 
 TEST_CASE("decode_with_three_rails", "") {
-    REQUIRE(rail_fence_cipher::decode("TEITELHDVLSNHDTISEIIEA", 3) == "THEDEVILISINTHEDETAILS");
+    REQUIRE(rail_fence_cipher::decode("TEITELHDVLSNHDTISEIIEA", 3) ==
+            "THEDEVILISINTHEDETAILS");
 }
 
 TEST_CASE("decode_with_five_rails", "") {
-    REQUIRE(rail_fence_cipher::decode("EIEXMSMESAORIWSCE", 5) == "EXERCISMISAWESOME");
+    REQUIRE(rail_fence_cipher::decode("EIEXMSMESAORIWSCE", 5) ==
+            "EXERCISMISAWESOME");
 }
 
 TEST_CASE("decode_with_six_rails", "") {
-    REQUIRE(rail_fence_cipher::decode("133714114238148966225439541018335470986172518171757571896261", 6)
-            == "112358132134558914423337761098715972584418167651094617711286");
+    REQUIRE(rail_fence_cipher::decode(
+                "133714114238148966225439541018335470986172518171757571896261",
+                6) ==
+            "112358132134558914423337761098715972584418167651094617711286");
 }
 
 #endif
