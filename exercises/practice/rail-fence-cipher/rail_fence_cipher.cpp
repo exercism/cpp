02c3a12ac60e5530ff4b46cf986999f3bdd48f22
Alexey Klimkin <klimkin@gmail.com>
2024-07-06 12:58:05 -0700

    feat: add practice exercise Rail Fence Cipher (#881)

---
+++ exercises/practice/rail-fence-cipher/rail_fence_cipher.cpp
@@ -0,0 +1,9 @@
+#include "rail_fence_cipher.h"
+
+namespace rail_fence_cipher {
+
+std::string encode(const std::string& plaintext, int num_rails) {}
+
+std::string decode(const std::string& ciphertext, int num_rails) {}
+
+}  // namespace rail_fence_cipher
