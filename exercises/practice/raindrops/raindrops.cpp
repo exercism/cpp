022d2153ee2ade62b78bf9dc677b369b04e53476
Shane Hamilton <graainneog@proton.me>
2024-09-04 19:38:07 +0200

    Update raindrops.cpp
    Fix raindrops exercise structure and add minimal guidance

--- exercises/practice/raindrops/raindrops.cpp
+++ exercises/practice/raindrops/raindrops.cpp
@@ -1,5 +1,7 @@
 #include "raindrops.h"
 
 namespace raindrops {
-
+    std::string convert(int num) {
+        // Implement your solution here, don't forget to update the header file and add the necessary import for strings
+    }
 }  // namespace raindrops
