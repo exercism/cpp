e50aadf61687b6a35f6587275f9248f29ea55aa5
Peter van der Heijden <peter@vanderheijden.name>
2024-10-11 19:44:35 +0200

    Run clang-tidy.

--- exercises/concept/last-will/last_will_test.cpp
+++ exercises/concept/last-will/last_will_test.cpp
@@ -8,9 +8,9 @@
 using namespace std;
 
 TEST_CASE("Family secrets have not been altered", "[task_1]") {
-    // We cannot test the existence of a namespace in the compiled 
+    // We cannot test the existence of a namespace in the compiled
     // Code.
-    // This test merely checks if the numbers in the file have 
+    // This test merely checks if the numbers in the file have
     // been changed. They have to be correct for the test to work.
 
     REQUIRE(zhang::bank_number_part(1) == 8541);
@@ -29,7 +29,8 @@ TEST_CASE("Family secrets have not been altered", "[task_1]") {
     REQUIRE(garcia::blue::code_fragment() == 923);
 }
 
-TEST_CASE("Account number assembly function exists in correct namespace", "[task_2]") {
+TEST_CASE("Account number assembly function exists in correct namespace",
+          "[task_2]") {
     REQUIRE_NOTHROW(estate_executor::assemble_account_number(0));
 }
 
@@ -39,11 +40,14 @@ TEST_CASE("Account number assembly works correctly", "[task_2]") {
     int account_with_secret_1{16706};
     int account_with_secret_23{14238};
 
-    REQUIRE(estate_executor::assemble_account_number(1) == account_with_secret_1);
-    REQUIRE(estate_executor::assemble_account_number(23) == account_with_secret_23);
+    REQUIRE(estate_executor::assemble_account_number(1) ==
+            account_with_secret_1);
+    REQUIRE(estate_executor::assemble_account_number(23) ==
+            account_with_secret_23);
 }
 
-TEST_CASE("Code fragment number assembly function exists in correct namespace", "[task_3]") {
+TEST_CASE("Code fragment number assembly function exists in correct namespace",
+          "[task_3]") {
     REQUIRE_NOTHROW(estate_executor::assemble_code());
 }
 
