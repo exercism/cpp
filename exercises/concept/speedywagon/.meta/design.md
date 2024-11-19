3e675fe9b2439492f1935629915a1488e669eb92
Christian Willner <34183939+vaeng@users.noreply.github.com>
2024-11-19 09:56:34 +0100

    feat: add exercise for pointer concept (#825)

---
+++ exercises/concept/speedywagon/.meta/design.md
@@ -0,0 +1,25 @@
+# Design
+
+## Goal
+
+The goal of this exercise is to teach the basics of pointers and how they work in C++.
+
+## Learning objectives
+
+- Know how to create a pointer from a variable
+- Know how to work with pointers to structs
+- Know how to work with array pointers
+
+## Out of scope
+
+- Smart pointers
+
+## Concepts
+
+The Concepts this exercise unlocks are:
+
+- `pointers`: how to work with pointers;
+
+## Prerequisites
+
+- `classes`
---
+++ exercises/concept/speedywagon/speedywagon.cpp
@@ -0,0 +1,21 @@
+#include "speedywagon.h"
+
+namespace speedywagon {
+
+// Enter your code below:
+
+// Please don't change the interface of the uv_light_heuristic function
+int uv_light_heuristic(std::vector<int>* data_array) {
+    double avg{};
+    for (auto element : *data_array) {
+        avg += element;
+    }
+    avg /= data_array->size();
+    int uv_index{};
+    for (auto element : *data_array) {
+        if (element > avg) ++uv_index;
+    }
+    return uv_index;
+}
+
+}  // namespace speedywagon
