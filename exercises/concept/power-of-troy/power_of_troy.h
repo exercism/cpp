7f4bcdebd0642e8949544b6115f1215eedde6738
vaeng <34183939+vaeng@users.noreply.github.com>
2025-01-23 11:34:57 +0100

    fix: make smart-pointer exercise easier

--- exercises/concept/power-of-troy/power_of_troy.h
+++ exercises/concept/power-of-troy/power_of_troy.h
@@ -5,10 +5,14 @@
 namespace troy {
 
 struct artifact {
+    // constructors needed (until C++20)
+    artifact(std::string name) : name(name) {}
     std::string name;
 };
 
 struct power {
+    // constructors needed (until C++20)
+    power(std::string effect) : effect(effect) {}
     std::string effect;
 };
 
