2babf30aee7d8bdbb365d57f85619724e5bc0a74
Marcel Weikum <marcel.weikum@wesodev.de>
2025-05-09 14:23:32 +0200

    Added book-store exercise to the track

---
+++ exercises/practice/book-store/.meta/example.h
@@ -0,0 +1,12 @@
+#ifndef BOOK_STORE_H
+#define BOOK_STORE_H
+
+#include <vector>
+
+namespace book_store {
+
+    int total(const std::vector<int>& basket);
+
+}  // namespace book_store
+
+#endif  // BOOK_STORE_H
