8686fa68fcddf10fbc7658003d33160965516305
Marcel Weikum <marcel.weikum@wesodev.de>
2025-05-12 09:29:05 +0200

    Added bottle-song exercise

---
+++ exercises/practice/bottle-song/.meta/example.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace bottle_song {
+
+    std::vector<std::string> recite(int startBottles, int takeDown);
+
+} // namespace bottle_song
---
+++ exercises/practice/bottle-song/bottle_song.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace bottle_song {
+
+// TODO: add your solution here
+
+}  // namespace bottle_song
