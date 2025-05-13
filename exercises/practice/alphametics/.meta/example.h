081002a8f068b7caca0e2f5bdf16656c0d2d9a2b
marcelweikum <marcel.weikum@wesodev.de>
2025-05-13 12:09:10 +0200

    Run pre-commit locally

--- exercises/practice/alphametics/.meta/example.h
+++ exercises/practice/alphametics/.meta/example.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <optional>
 #include <map>
+#include <optional>
 #include <string>
 
 namespace alphametics {
 
-    std::optional<std::map<char,int>> solve(const std::string& puzzle);
+std::optional<std::map<char, int>> solve(const std::string& puzzle);
 
-} // namespace alphametics
+}  // namespace alphametics
