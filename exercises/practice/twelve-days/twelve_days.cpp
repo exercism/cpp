ada7fd856eb55763cfb3f7ff3e00e83bba223089
alienx5499 <p2005p5499p@gmail.com>
2025-08-29 23:48:05 +0530

    Remove stub function signatures from twelve-days exercise

--- exercises/practice/twelve-days/twelve_days.cpp
+++ exercises/practice/twelve-days/twelve_days.cpp
@@ -4,19 +4,4 @@
 
 namespace twelve_days {
 
-std::string verse(int day) {
-    // TODO: Implement the verse function
-    return "";
-}
-
-std::string lyrics(int start_day, int end_day) {
-    // TODO: Implement the lyrics function with start and end days
-    return "";
-}
-
-std::string lyrics(int end_day) {
-    // TODO: Implement the lyrics function from day 1 to end_day
-    return lyrics(1, end_day);
-}
-
 }  // namespace twelve_days
--- exercises/practice/twelve-days/twelve_days.h
+++ exercises/practice/twelve-days/twelve_days.h
@@ -4,8 +4,4 @@
 
 namespace twelve_days {
 
-std::string verse(int day);
-std::string lyrics(int start_day, int end_day);
-std::string lyrics(int end_day);
-
 }  // namespace twelve_days
